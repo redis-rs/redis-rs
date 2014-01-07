8fa5df1df78b697505595cc45e1c2c43b9dd832c
Lucian Branescu Mihaila <lucian.branescu@gmail.com>
2014-01-07 18:41:05 +0000

    Working execute and slightly more useful getset.

--- src/redis/test.rs
+++ src/redis/test.rs
@@ -13,9 +13,28 @@ fn ping() {
 }
 
 #[test]
-fn get_set() {
+fn get_set_exists() {
 	let mut client = prepare();
-	assert!(client.get("k").is_none());	
-	assert!(client.set("k", "v"));
-	assert!(client.get("k") == Some(~"v"));
+	assert!(client.get("foo").is_none());	
+
+	assert!(client.set("foo", "test"));
+	assert!(client.get("foo") == Some(~"test"));
+	assert!(client.exists("foo"));
+	assert!(client.get_type("foo") == StringType);
+
+	assert!(client.set("bar", 42));
+	assert!(client.get_as::<int>("bar") == Some(42));
+	assert!(client.get_type("bar") == StringType);
+}
+
+#[test]
+fn script() {
+	let mut client = prepare();
+	let script = redis::Script::new("
+        return tonumber(ARGV[1]);
+    ");
+
+	assert!(
+		client.call_script(&script, [], [redis::StrArg("42")]) == redis::Int(42)
+	);
 }
