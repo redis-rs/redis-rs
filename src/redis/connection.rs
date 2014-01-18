89be6663aab287ee7c48c52ca0f81295fbcbb0da
Armin Ronacher <armin.ronacher@active-4.com>
2014-01-17 23:31:46 -0800

    More commands

--- src/redis/connection.rs
+++ src/redis/connection.rs
@@ -37,6 +37,24 @@ fn value_to_string_list(val: &Value) -> ~[~str] {
     }
 }
 
+fn value_to_byte_list(val: &Value) -> ~[~[u8]] {
+    match *val {
+        Bulk(ref items) => {
+            let mut rv = ~[];
+            for item in items.iter() {
+                match item {
+                    &Data(ref payload) => {
+                        rv.push(payload.to_owned());
+                    },
+                    _ => {}
+                }
+            }
+            rv
+        },
+        _ => ~[],
+    }
+}
+
 fn value_to_key_value_tuple(val: &Value) -> Option<(~str, ~[u8])> {
     match *val {
         Bulk(ref items) => {
@@ -378,6 +396,7 @@ impl Connection {
 
     // -- key / value commands
 
+    #[inline]
     pub fn get_bytes(&mut self, key: &str) -> Option<~[u8]> {
         match self.execute("GET", [StrArg(key)]) {
             Data(value) => Some(value),
@@ -385,6 +404,7 @@ impl Connection {
         }
     }
 
+    #[inline]
     pub fn get(&mut self, key: &str) -> Option<~str> {
         match self.get_bytes(key) {
             None => None,
@@ -392,6 +412,7 @@ impl Connection {
         }
     }
 
+    #[inline]
     pub fn get_as<T: FromStr>(&mut self, key: &str) -> Option<T> {
         match self.get(key) {
             None => None,
@@ -399,6 +420,7 @@ impl Connection {
         }
     }
 
+    #[inline]
     pub fn set_bytes(&mut self, key: &str, value: &[u8]) -> bool {
         match self.execute("SET", [StrArg(key), BytesArg(value)]) {
             Success => true,
@@ -406,6 +428,7 @@ impl Connection {
         }
     }
 
+    #[inline]
     pub fn set<T: ToStr>(&mut self, key: &str, value: T) -> bool {
         let v = value.to_str();
         match self.execute("SET", [StrArg(key), StrArg(v)]) {
@@ -414,6 +437,7 @@ impl Connection {
         }
     }
 
+    #[inline]
     pub fn setex_bytes(&mut self, key: &str, value: &[u8], timeout: f32) -> bool {
         let mut cmd;
         let mut t;
@@ -431,11 +455,13 @@ impl Connection {
         }
     }
 
+    #[inline]
     pub fn setex<T: ToStr>(&mut self, key: &str, value: T, timeout: f32) -> bool {
         let v = value.to_str();
         self.setex_bytes(key, v.as_bytes(), timeout)
     }
 
+    #[inline]
     pub fn setnx_bytes(&mut self, key: &str, value: &[u8]) -> bool {
         match self.execute("SETNX", [StrArg(key), BytesArg(value)]) {
             Int(1) => true,
@@ -443,6 +469,7 @@ impl Connection {
         }
     }
 
+    #[inline]
     pub fn setnx<T: ToStr>(&mut self, key: &str, value: T) -> bool {
         let v = value.to_str();
         match self.execute("SETNX", [StrArg(key), StrArg(v)]) {
@@ -451,6 +478,7 @@ impl Connection {
         }
     }
 
+    #[inline]
     pub fn del(&mut self, key: &str) -> bool {
         match self.execute("DEL", [StrArg(key)]) {
             Int(0) | Nil => false,
@@ -459,6 +487,7 @@ impl Connection {
         }
     }
 
+    #[inline]
     pub fn del_many(&mut self, keys: &[&str]) -> uint {
         let args = keys.iter().map(|&x| StrArg(x)).to_owned_vec();
         match self.execute("DEL", args) {
@@ -467,6 +496,7 @@ impl Connection {
         }
     }
 
+    #[inline]
     pub fn getset_bytes(&mut self, key: &str, value: &[u8]) -> Option<~[u8]> {
         match self.execute("GETSET", [StrArg(key), BytesArg(value)]) {
             Data(value) => Some(value),
@@ -474,21 +504,16 @@ impl Connection {
         }
     }
 
-    pub fn getset(&mut self, key: &str, value: ~str) -> Option<~str> {
-        match self.execute("GETSET", [StrArg(key), StrArg(value)]) {
-            Data(x) => Some(from_utf8_owned(x)),
-            _ => None,
-        }
-    }
-
-    pub fn getset_as<T: ToStr+FromStr>(&mut self, key: &str, value: T) -> Option<T> {
+    #[inline]
+    pub fn getset<T: ToStr+FromStr>(&mut self, key: &str, value: T) -> Option<T> {
         let v = value.to_str();
-        match self.getset(key, v) {
-            Some(x) => from_str(x),
+        match self.getset_bytes(key, v.as_bytes()) {
+            Some(x) => from_str(from_utf8_owned(x)),
             None => None,
         }
     }
 
+    #[inline]
     pub fn append_bytes(&mut self, key: &str, value: &[u8]) -> uint {
         match self.execute("APPEND", [StrArg(key), BytesArg(value)]) {
             Int(x) => x as uint,
@@ -496,11 +521,13 @@ impl Connection {
         }
     }
 
+    #[inline]
     pub fn append<T: ToStr>(&mut self, key: &str, value: T) -> uint {
         let v = value.to_str();
         self.append_bytes(key, v.as_bytes())
     }
 
+    #[inline]
     pub fn setbit(&mut self, key: &str, bit: uint, value: bool) -> bool {
         let v = if value { 1 } else { 0 };
         match self.execute("SETBIT", [StrArg(key), IntArg(bit as int), IntArg(v)]) {
@@ -509,6 +536,7 @@ impl Connection {
         }
     }
 
+    #[inline]
     pub fn strlen(&mut self, key: &str) -> uint {
         match self.execute("STRLEN", [StrArg(key)]) {
             Int(x) => x as uint,
@@ -516,6 +544,7 @@ impl Connection {
         }
     }
 
+    #[inline]
     pub fn getrange_bytes(&mut self, key: &str, start: int, end: int) -> ~[u8] {
         match self.execute("GETRANGE", [StrArg(key), IntArg(start), IntArg(end)]) {
             Data(value) => value,
@@ -523,10 +552,12 @@ impl Connection {
         }
     }
 
+    #[inline]
     pub fn getrange(&mut self, key: &str, start: int, end: int) -> ~str {
         from_utf8_owned(self.getrange_bytes(key, start, end))
     }
 
+    #[inline]
     pub fn setrange_bytes(&mut self, key: &str, offset: int, value: &[u8]) -> uint {
         match self.execute("SETRANGE", [StrArg(key), IntArg(offset), BytesArg(value)]) {
             Int(x) => x as uint,
@@ -534,10 +565,12 @@ impl Connection {
         }
     }
 
+    #[inline]
     pub fn setrange(&mut self, key: &str, offset: int, value: &str) -> uint {
         self.setrange_bytes(key, offset, value.as_bytes())
     }
 
+    #[inline]
     pub fn popcount(&mut self, key: &str) -> uint {
         match self.execute("POPCOUNT", [StrArg(key)]) {
             Int(x) => x as uint,
@@ -545,6 +578,7 @@ impl Connection {
         }
     }
 
+    #[inline]
     pub fn popcount_range(&mut self, key: &str, start: int, end: int) -> uint {
         match self.execute("POPCOUNT", [StrArg(key), IntArg(start), IntArg(end)]) {
             Int(x) => x as uint,
@@ -552,6 +586,7 @@ impl Connection {
         }
     }
 
+    #[inline]
     pub fn incr(&mut self, key: &str) -> int {
         match self.execute("INCR", [StrArg(key)]) {
             Int(x) => x as int,
@@ -559,6 +594,7 @@ impl Connection {
         }
     }
 
+    #[inline]
     pub fn incrby(&mut self, key: &str, step: int) -> int {
         match self.execute("INCRBY", [StrArg(key), IntArg(step)]) {
             Int(x) => x as int,
@@ -566,6 +602,7 @@ impl Connection {
         }
     }
 
+    #[inline]
     pub fn incrby_float(&mut self, key: &str, step: f32) -> f32 {
         match self.execute("INCRBYFLOAT", [StrArg(key), FloatArg(step)]) {
             Data(x) => {
@@ -578,6 +615,7 @@ impl Connection {
         }
     }
 
+    #[inline]
     pub fn decr(&mut self, key: &str) -> int {
         match self.execute("DECR", [StrArg(key)]) {
             Int(x) => x as int,
@@ -585,6 +623,7 @@ impl Connection {
         }
     }
 
+    #[inline]
     pub fn decrby(&mut self, key: &str, step: int) -> int {
         match self.execute("DECRBY", [StrArg(key), IntArg(step)]) {
             Int(x) => x as int,
@@ -592,10 +631,12 @@ impl Connection {
         }
     }
 
+    #[inline]
     pub fn decrby_float(&mut self, key: &str, step: f32) -> f32 {
         self.incrby_float(key, -step)
     }
 
+    #[inline]
     pub fn exists(&mut self, key: &str) -> bool {
         match self.execute("EXISTS", [StrArg(key)]) {
             Int(1) => true,
@@ -605,6 +646,7 @@ impl Connection {
 
     // -- list commands
 
+    #[inline]
     fn blocking_pop_bytes(&mut self, cmd: &str, keys: &[&str],
                           timeout: f32) -> Option<(~str, ~[u8])> {
         let mut timeout_s = timeout as int;
@@ -616,10 +658,12 @@ impl Connection {
         value_to_key_value_tuple(&self.execute(cmd, args))
     }
 
+    #[inline]
     pub fn blpop_bytes(&mut self, keys: &[&str], timeout: f32) -> Option<(~str, ~[u8])> {
         self.blocking_pop_bytes("BLPOP", keys, timeout)
     }
 
+    #[inline]
     pub fn blpop(&mut self, keys: &[&str], timeout: f32) -> Option<(~str, ~str)> {
         match self.blpop_bytes(keys, timeout) {
             Some((key, value)) => {
@@ -629,6 +673,7 @@ impl Connection {
         }
     }
 
+    #[inline]
     pub fn blpop_as<T: FromStr>(&mut self, keys: &[&str], timeout: f32) -> Option<(~str, T)> {
         match self.blpop(keys, timeout) {
             Some((key, value)) => {
@@ -638,10 +683,12 @@ impl Connection {
         }
     }
 
+    #[inline]
     pub fn brpop_bytes(&mut self, keys: &[&str], timeout: f32) -> Option<(~str, ~[u8])> {
         self.blocking_pop_bytes("BRPOP", keys, timeout)
     }
 
+    #[inline]
     pub fn brpop(&mut self, keys: &[&str], timeout: f32) -> Option<(~str, ~str)> {
         match self.brpop_bytes(keys, timeout) {
             Some((key, value)) => {
@@ -651,6 +698,7 @@ impl Connection {
         }
     }
 
+    #[inline]
     pub fn brpop_as<T: FromStr>(&mut self, keys: &[&str], timeout: f32) -> Option<(~str, T)> {
         match self.brpop(keys, timeout) {
             Some((key, value)) => {
@@ -660,6 +708,7 @@ impl Connection {
         }
     }
 
+    #[inline]
     pub fn brpoplpush_bytes(&mut self, src: &str, dst: &str, timeout: f32) -> Option<~[u8]> {
         let mut timeout_s = timeout as int;
         if (timeout_s <= 0) {
@@ -672,18 +721,15 @@ impl Connection {
         }
     }
 
-    pub fn rpush_bytes(&mut self, key: &str, value: &[u8]) -> uint {
-        match self.execute("RPUSH", [StrArg(key), BytesArg(value)]) {
-            Int(x) => x as uint,
-            _ => 0,
+    #[inline]
+    pub fn brpoplpush(&mut self, src: &str, dst: &str, timeout: f32) -> Option<~str> {
+        match self.brpoplpush_bytes(src, dst, timeout) {
+            Some(x) => Some(from_utf8_owned(x)),
+            None => None,
         }
     }
 
-    pub fn rpush<T: ToStr>(&mut self, key: &str, value: T) -> uint {
-        let v = value.to_str();
-        self.rpush_bytes(key, v.as_bytes())
-    }
-
+    #[inline]
     pub fn lindex_bytes(&mut self, key: &str, index: i64) -> Option<~[u8]> {
         match self.execute("LINDEX", [StrArg(key), IntArg(index as int)]) {
             Data(value) => Some(value),
@@ -691,6 +737,7 @@ impl Connection {
         }
     }
 
+    #[inline]
     pub fn lindex(&mut self, key: &str, index: i64) -> Option<~str> {
         match self.lindex_bytes(key, index) {
             Some(x) => Some(from_utf8_owned(x)),
@@ -698,10 +745,214 @@ impl Connection {
         }
     }
 
+    #[inline]
     pub fn lindex_as<T: FromStr>(&mut self, key: &str, index: i64) -> Option<T> {
         match self.lindex(key, index) {
             Some(x) => from_str(x),
             None => None,
         }
+    }
+
+    #[inline]
+    fn list_insert_operation(&mut self, cmd: &str, key: &str, pivot: &[u8], value: &[u8]) -> int {
+        match self.execute("LINSERT", [StrArg(cmd), StrArg(key), StrArg("BEFORE"),
+                                       BytesArg(pivot), BytesArg(value)]) {
+            Int(value) => value as int,
+            _ => 0,
+        }
+    }
+
+    #[inline]
+    pub fn linsert_before_bytes(&mut self, key: &str, pivot: &[u8], value: &[u8]) -> int {
+        self.list_insert_operation("BEFORE", key, pivot, value)
+    }
+
+    #[inline]
+    pub fn linsert_before<T: ToStr>(&mut self, key: &str, pivot: &str, value: T) -> int {
+        let v = value.to_str();
+        self.linsert_before_bytes(key, pivot.as_bytes(), v.as_bytes())
+    }
+
+    #[inline]
+    pub fn linsert_after_bytes(&mut self, key: &str, pivot: &[u8], value: &[u8]) -> int {
+        self.list_insert_operation("AFTER", key, pivot, value)
+    }
+
+    #[inline]
+    pub fn linsert_after<T: ToStr>(&mut self, key: &str, pivot: &str, value: T) -> int {
+        let v = value.to_str();
+        self.linsert_after_bytes(key, pivot.as_bytes(), v.as_bytes())
+    }
+
+    #[inline]
+    pub fn llen(&mut self, key: &str) -> uint {
+        match self.execute("LLEN", [StrArg(key)]) {
+            Int(x) => x as uint,
+            _ => 0,
+        }
+    }
+
+    #[inline]
+    pub fn lpop_bytes(&mut self, key: &str) -> Option<~[u8]> {
+        match self.execute("LPOP", [StrArg(key)]) {
+            Data(payload) => Some(payload),
+            _ => None,
+        }
+    }
+
+    #[inline]
+    pub fn lpop(&mut self, key: &str) -> Option<~str> {
+        match self.lpop_bytes(key) {
+            Some(x) => Some(from_utf8_owned(x)),
+            None => None,
+        }
+    }
+
+    #[inline]
+    pub fn lpop_as<T: FromStr>(&mut self, key: &str) -> Option<T> {
+        match self.lpop(key) {
+            Some(x) => from_str(x),
+            None => None,
+        }
+    }
+
+    #[inline]
+    pub fn lpush_bytes(&mut self, key: &str, value: &[u8]) -> uint {
+        match self.execute("LPUSH", [StrArg(key), BytesArg(value)]) {
+            Int(x) => x as uint,
+            _ => 0,
+        }
+    }
+
+    #[inline]
+    pub fn lpush<T: ToStr>(&mut self, key: &str, value: T) -> uint {
+        let v = value.to_str();
+        self.lpush_bytes(key, v.as_bytes())
+    }
+
+    #[inline]
+    pub fn lpushx_bytes(&mut self, key: &str, value: &[u8]) -> uint {
+        match self.execute("LPUSHX", [StrArg(key), BytesArg(value)]) {
+            Int(x) => x as uint,
+            _ => 0,
+        }
+    }
+
+    #[inline]
+    pub fn lpushx<T: ToStr>(&mut self, key: &str, value: T) -> uint {
+        let v = value.to_str();
+        self.lpushx_bytes(key, v.as_bytes())
+    }
+
+    #[inline]
+    pub fn lrange_bytes(&mut self, key: &str, start: int, end: int) -> ~[~[u8]] {
+        value_to_byte_list(&self.execute("LRANGE", [StrArg(key), IntArg(start), IntArg(end)]))
+    }
+
+    #[inline]
+    pub fn lrange(&mut self, key: &str, start: int, end: int) -> ~[~str] {
+        let items = self.lrange_bytes(key, start, end);
+        items.move_iter().map(|x| from_utf8_owned(x)).to_owned_vec()
+    }
+
+    #[inline]
+    pub fn lrange_as<T: FromStr>(&mut self, key: &str, start: int, end: int) -> ~[T] {
+        let items = self.lrange(key, start, end);
+        let mut rv = ~[];
+        for item in items.move_iter() {
+            match from_str(item) {
+                Some(x) => { rv.push(x); }
+                None => {}
+            };
+        }
+        rv
+    }
+
+    #[inline]
+    pub fn lrem_bytes(&mut self, key: &str, count: int, value: &[u8]) -> uint {
+        match self.execute("LREM", [StrArg(key), IntArg(count), BytesArg(value)]) {
+            Int(x) => x as uint,
+            _ => 0,
+        }
+    }
+
+    #[inline]
+    pub fn lrem<T: ToStr>(&mut self, key: &str, count: int, value: T) -> uint {
+        let v = value.to_str();
+        self.lrem_bytes(key, count, v.as_bytes())
+    }
+
+    #[inline]
+    pub fn lset_bytes(&mut self, key: &str, index: int, value: &[u8]) -> bool {
+        match self.execute("LSET", [StrArg(key), IntArg(index), BytesArg(value)]) {
+            Success => true,
+            _ => true,
+        }
+    }
+
+    #[inline]
+    pub fn lset<T: ToStr>(&mut self, key: &str, index: int, value: T) -> bool {
+        let v = value.to_str();
+        self.lset_bytes(key, index, v.as_bytes())
+    }
+
+    #[inline]
+    pub fn ltrim(&mut self, key: &str, start: int, stop: int) -> bool {
+        match self.execute("LTRIM", [StrArg(key), IntArg(start), IntArg(stop)]) {
+            Success => true,
+            _ => false,
+        }
+    }
+
+    #[inline]
+    pub fn rpop_bytes(&mut self, key: &str) -> Option<~[u8]> {
+        match self.execute("RPOP", [StrArg(key)]) {
+            Data(payload) => Some(payload),
+            _ => None,
+        }
+    }
+
+    #[inline]
+    pub fn rpop(&mut self, key: &str) -> Option<~str> {
+        match self.rpop_bytes(key) {
+            Some(x) => Some(from_utf8_owned(x)),
+            None => None,
+        }
+    }
+
+    #[inline]
+    pub fn rpop_as<T: FromStr>(&mut self, key: &str) -> Option<T> {
+        match self.rpop(key) {
+            Some(x) => from_str(x),
+            None => None,
+        }
+    }
+
+    #[inline]
+    pub fn rpush_bytes(&mut self, key: &str, value: &[u8]) -> uint {
+        match self.execute("RPUSH", [StrArg(key), BytesArg(value)]) {
+            Int(x) => x as uint,
+            _ => 0,
+        }
+    }
+
+    #[inline]
+    pub fn rpush<T: ToStr>(&mut self, key: &str, value: T) -> uint {
+        let v = value.to_str();
+        self.rpush_bytes(key, v.as_bytes())
+    }
+
+    #[inline]
+    pub fn rpushx_bytes(&mut self, key: &str, value: &[u8]) -> uint {
+        match self.execute("RPUSHX", [StrArg(key), BytesArg(value)]) {
+            Int(x) => x as uint,
+            _ => 0,
+        }
+    }
+
+    #[inline]
+    pub fn rpushx<T: ToStr>(&mut self, key: &str, value: T) -> uint {
+        let v = value.to_str();
+        self.rpushx_bytes(key, v.as_bytes())
     }
 }
