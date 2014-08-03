95c95b85b1ed919c0d9e532fd5552d5b09d05ca9
Bilal Husain <bilal@bilalhusain.com>
2014-08-03 18:07:38 +0000

    to new inner attribute syntax

--- src/redis/lib.rs
+++ src/redis/lib.rs
@@ -1,11 +1,11 @@
-#[crate_id = "redis#0.1"];
-#[crate_type = "lib"];
-#[license = "BSD"];
-#[comment = "Bindings and wrapper functions for redis."];
+#![crate_id = "redis#0.1"]
+#![crate_type = "lib"]
+#![license = "BSD"]
+#![comment = "Bindings and wrapper functions for redis."]
 
-#[deny(non_camel_case_types)];
-#[feature(macro_rules)];
-#[feature(globs)];
+#![deny(non_camel_case_types)]
+#![feature(macro_rules)]
+#![feature(globs)]
 
 extern crate extra;
 extern crate time;
