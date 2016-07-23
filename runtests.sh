7ecf0e77e3c638a7aa5e84818ff90e723a61658e
Armin Ronacher <armin.ronacher@active-4.com>
2016-07-23 11:27:28 +0200

    Make tests during development faster by not sharing the target folder

--- runtests.sh
+++ runtests.sh
@@ -3,18 +3,22 @@
 test_run() {
   echo "======================================================================"
   echo -n "Testing Connection Type '$1'"
-  if [ x$2 != x ]; then
-    echo " [features=$2]"
+  if [ x$3 != x ]; then
+    echo " [features=$3]"
   else
     echo
   fi
   echo "======================================================================"
-  echo
-  RUST_TEST_THREADS=1 REDISRS_SERVER_TYPE=$1 cargo test --features="with-rustc-json $2"
+
+  if [ x$REDISRS_UNIFIED_TARGET != x1 ]; then
+    export CARGO_TARGET_DIR=target/test-$2
+  fi
+
+  RUST_TEST_THREADS=1 REDISRS_SERVER_TYPE=$1 cargo test --features="with-rustc-json $3"
 }
 
-test_run "tcp"
+test_run "tcp" "tcp-basic"
 if grep with-system-unix-sockets target/debug/build/redis-*/output -q; then
-  test_run "unix"
+  test_run "unix" "unix-basic"
 fi
-test_run "unix" "with-unix-sockets"
+test_run "unix" "unix-dep" "with-unix-sockets"
