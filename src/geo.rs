aeda474f3dfdfbe11a57495049e0e55b6817111d
Ayose <ayosec@gmail.com>
2017-07-09 06:10:00 +0100

    Builder for GEORADIUS command

--- src/geo.rs
+++ src/geo.rs
@@ -16,12 +16,13 @@ pub enum Unit {
 
 impl ToRedisArgs for Unit {
     fn to_redis_args(&self) -> Vec<Vec<u8>> {
-        match *self {
-            Unit::Meters => vec![vec![b'm']],
-            Unit::Kilometers => vec![vec![b'k', b'm']],
-            Unit::Miles => vec![vec![b'm', b'i']],
-            Unit::Feet => vec![vec![b'f', b't']],
-        }
+        let args = match *self {
+            Unit::Meters => "m",
+            Unit::Kilometers => "km",
+            Unit::Miles => "mi",
+            Unit::Feet => "ft",
+        };
+        vec![args.as_bytes().to_vec()]
     }
 }
 
@@ -63,8 +64,134 @@ impl<T: FromRedisValue> FromRedisValue for Coord<T> {
 impl<T: ToRedisArgs> ToRedisArgs for Coord<T> {
     fn to_redis_args(&self) -> Vec<Vec<u8>> {
         let mut args = Vec::new();
-        args.extend_from_slice(&ToRedisArgs::to_redis_args(&self.longitude));
-        args.extend_from_slice(&ToRedisArgs::to_redis_args(&self.latitude));
+        for field in &[&self.longitude, &self.latitude] {
+            args.extend(ToRedisArgs::to_redis_args(*field));
+        }
+        args
+    }
+}
+
+/// Options to sort results from GEORADIUS and GEORADIUSBYMEMBER commands
+pub enum RadiusOrder {
+    /// Don't sort the results
+    Unsorted,
+
+    /// Sort returned items from the nearest to the farthest, relative to the center.
+    Asc,
+
+    /// Sort returned items from the farthest to the nearest, relative to the center.
+    Desc,
+}
+
+impl Default for RadiusOrder {
+    fn default() -> RadiusOrder {
+        RadiusOrder::Unsorted
+    }
+}
+
+/// Options for the GEORADIUS and GEORADIUSBYMEMBER commands
+#[derive(Default)]
+pub struct RadiusOptions {
+    with_coord: bool,
+    with_dist: bool,
+    with_hash: bool,
+    count: Option<usize>,
+    order: RadiusOrder,
+    store: Option<Vec<Vec<u8>>>,
+    store_dist: Option<Vec<Vec<u8>>>,
+}
+
+impl RadiusOptions {
+
+    /// Limit the results to the first N matching items.
+    pub fn limit(&mut self, n: usize) -> &mut Self {
+        self.count = Some(n);
+        self
+    }
+
+    /// Return the distance of the returned items from the specified center.
+    /// The distance is returned in the same unit as the unit specified as the
+    /// radius argument of the command.
+    pub fn with_dist(&mut self) -> &mut Self {
+        self.with_dist = true;
+        self
+    }
+
+    /// Return the longitude,latitude coordinates of the matching items.
+    pub fn with_coord(&mut self) -> &mut Self {
+        self.with_coord = true;
+        self
+    }
+
+    /// Return the raw geohash-encoded sorted set score of the item, in the
+    /// form of a 52 bit unsigned integer. This is only useful for low level
+    /// hacks or debugging and is otherwise of little interest for the general
+    /// user.
+    pub fn with_hash(&mut self) -> &mut Self {
+        self.with_hash = true;
+        self
+    }
+
+    /// Sort the returned items
+    pub fn order(&mut self, o: RadiusOrder) -> &mut Self {
+        self.order = o;
+        self
+    }
+
+    /// Store the results in a sorted set at key, instead of returning them.
+    ///
+    /// This feature can't be used with any `with_*` method.
+    pub fn store<K: ToRedisArgs>(&mut self, key: K) -> &mut Self {
+        self.store = Some(ToRedisArgs::to_redis_args(&key));
+        self
+    }
+
+    /// Similar to `store`, but includes the distance of every member from the
+    /// center. This feature can't be used with any `with_*` method.
+    pub fn store_dist<K: ToRedisArgs>(&mut self, key: K) -> &mut Self {
+        self.store_dist = Some(ToRedisArgs::to_redis_args(&key));
+        self
+    }
+
+}
+
+impl ToRedisArgs for RadiusOptions {
+    fn to_redis_args(&self) -> Vec<Vec<u8>> {
+        let mut args = Vec::new();
+
+        if self.with_coord {
+            args.push("WITHCOORD".as_bytes().to_vec());
+        }
+
+        if self.with_dist {
+            args.push("WITHDIST".as_bytes().to_vec());
+        }
+
+        if self.with_hash {
+            args.push("WITHHASH".as_bytes().to_vec());
+        }
+
+        if let Some(n) = self.count {
+            args.push("COUNT".as_bytes().to_vec());
+            args.push(format!("{}", n).into_bytes());
+        }
+
+        match self.order {
+            RadiusOrder::Asc => args.push("ASC".as_bytes().to_vec()),
+            RadiusOrder::Desc => args.push("DESC".as_bytes().to_vec()),
+            _ => (),
+        };
+
+        if let Some(ref store) = self.store {
+            args.push("STORE".as_bytes().to_vec());
+            args.extend_from_slice(&store[..]);
+        }
+
+        if let Some(ref store_dist) = self.store_dist {
+            args.push("STOREDIST".as_bytes().to_vec());
+            args.extend_from_slice(&store_dist[..]);
+        }
+
         args
     }
 }
@@ -72,18 +199,51 @@ impl<T: ToRedisArgs> ToRedisArgs for Coord<T> {
 #[cfg(test)]
 mod tests {
     use types::ToRedisArgs;
-    use super::Coord;
+    use super::{Coord, RadiusOptions, RadiusOrder};
     use std::str;
+
+    macro_rules! assert_args {
+        ($value:expr, $($args:expr),+) => {
+            let args = ToRedisArgs::to_redis_args($value);
+            let strings: Vec<_> = args.iter()
+                                      .map(|a| str::from_utf8(a.as_ref()).unwrap())
+                                      .collect();
+            assert_eq!(strings, vec![$($args),+]);
+        }
+    }
 
     #[test]
     fn test_coord_to_args() {
         let member = ("Palermo", Coord::lon_lat("13.361389", "38.115556"));
-        let args = ToRedisArgs::to_redis_args(&member);
-        let mut strings = args.iter().map(|c| str::from_utf8(c).unwrap());
+        assert_args!(&member, "Palermo", "13.361389", "38.115556");
+    }
 
-        assert_eq!(strings.next(), Some("Palermo"));
-        assert_eq!(strings.next(), Some("13.361389"));
-        assert_eq!(strings.next(), Some("38.115556"));
-        assert_eq!(strings.next(), None);
+    #[test]
+    fn test_radius_options() {
+        // Without options, should not generate any argument
+        let empty = RadiusOptions::default();
+        assert_eq!(ToRedisArgs::to_redis_args(&empty).len(), 0);
+
+        // Some combinations with WITH* options
+        let opts = || RadiusOptions::default();
+
+        assert_args!(
+            opts().with_coord().with_dist(),
+            "WITHCOORD", "WITHDIST");
+
+        assert_args!(opts().limit(50), "COUNT", "50");
+
+        assert_args!(
+            opts().limit(50).store("x"),
+            "COUNT", "50", "STORE", "x");
+
+        assert_args!(
+            opts().limit(100).store_dist("y"),
+            "COUNT", "100", "STOREDIST", "y");
+
+        assert_args!(
+            opts().order(RadiusOrder::Asc).limit(10).with_dist(),
+            "WITHDIST", "COUNT", "10", "ASC");
+
     }
 }
