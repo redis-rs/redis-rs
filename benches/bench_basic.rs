e733121a103fe8e4561cae5145ad3dbfedf2a054
Markus Westerlind <markus.westerlind@distilnetworks.com>
2018-01-19 14:02:55 +0100

    Add a benchmark for sending longer pipelines

--- benches/bench_basic.rs
+++ benches/bench_basic.rs
@@ -4,6 +4,8 @@ extern crate bencher;
 
 use bencher::Bencher;
 
+use redis::PipelineCommands;
+
 fn get_client() -> redis::Client {
     redis::Client::open("redis://127.0.0.1:6379").unwrap()
 }
@@ -61,11 +63,26 @@ fn bench_simple_getsetdel_pipeline_precreated(b: &mut Bencher) {
     });
 }
 
+fn bench_long_pipeline(b: &mut Bencher) {
+    let client = get_client();
+    let con = client.get_connection().unwrap();
+    let mut pipe = redis::pipe();
+
+    for _ in 0..1_000 {
+        pipe.set("foo", "bar").ignore();
+    }
+
+    b.iter(|| {
+        let _: () = pipe.query(&con).unwrap();
+    });
+}
+
 
 benchmark_group!(
     bench,
     bench_simple_getsetdel,
     bench_simple_getsetdel_pipeline,
-    bench_simple_getsetdel_pipeline_precreated
+    bench_simple_getsetdel_pipeline_precreated,
+    bench_long_pipeline
 );
 benchmark_main!(bench);
