5eefdd810c854adf388fd6e2b1b3fe6c5d11d2e0
Matthew Collier <mcollier@digitalbazaar.com>
2020-01-05 18:16:43 -0500

    Does not compile due to lifetime.

--- examples/async-multiplexed.rs
+++ examples/async-multiplexed.rs
@@ -3,34 +3,32 @@ extern crate redis;
 use futures::{future, prelude::*};
 use redis::{aio::MultiplexedConnection, RedisResult};
 
-fn test_cmd(con: &MultiplexedConnection, i: i32) -> impl Future<Output = RedisResult<()>> + Send {
+async fn test_cmd(con: &MultiplexedConnection, i: i32) -> RedisResult<()> {
     let mut con = con.clone();
-    async move {
-        let key = format!("key{}", i);
-        let key_2 = key.clone();
-        let key2 = format!("key{}_2", i);
-        let key2_2 = key2.clone();
+    let key = format!("key{}", i);
+    let key_2 = key.clone();
+    let key2 = format!("key{}_2", i);
+    let key2_2 = key2.clone();
 
-        let foo_val = format!("foo{}", i);
+    let foo_val = format!("foo{}", i);
 
-        let () = redis::cmd("SET")
-            .arg(&key[..])
-            .arg(foo_val.as_bytes())
-            .query_async(&mut con)
-            .await?;
-        let () = redis::cmd("SET")
-            .arg(&[&key2, "bar"])
-            .query_async(&mut con)
-            .await?;
-        redis::cmd("MGET")
-            .arg(&[&key_2, &key2_2])
-            .query_async(&mut con)
-            .map(|result| {
-                assert_eq!(Ok((foo_val, b"bar".to_vec())), result);
-                Ok(())
-            })
-            .await
-    }
+    let () = redis::cmd("SET")
+        .arg(&key[..])
+        .arg(foo_val.as_bytes())
+        .query_async(&mut con)
+        .await?;
+    let () = redis::cmd("SET")
+        .arg(&[&key2, "bar"])
+        .query_async(&mut con)
+        .await?;
+    redis::cmd("MGET")
+        .arg(&[&key_2, &key2_2])
+        .query_async(&mut con)
+        .map(|result| {
+            assert_eq!(Ok((foo_val, b"bar".to_vec())), result);
+            Ok(())
+        })
+        .await
 }
 
 #[tokio::main]
