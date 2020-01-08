4d493f5533a52b4d638acdc2940501557bab819b
Jan-Erik Rediger <janerik@fnordig.de>
2020-01-08 18:43:26 +0100

    Cleanup multiplexed async example

--- examples/async-multiplexed.rs
+++ examples/async-multiplexed.rs
@@ -1,31 +1,29 @@
-extern crate futures;
-extern crate redis;
 use futures::{future, prelude::*};
 use redis::{aio::MultiplexedConnection, RedisResult};
 
 async fn test_cmd(con: &MultiplexedConnection, i: i32) -> RedisResult<()> {
     let mut con = con.clone();
+
     let key = format!("key{}", i);
-    let key_2 = key.clone();
     let key2 = format!("key{}_2", i);
-    let key2_2 = key2.clone();
+    let value = format!("foo{}", i);
 
-    let foo_val = format!("foo{}", i);
-
-    let () = redis::cmd("SET")
+    redis::cmd("SET")
         .arg(&key[..])
-        .arg(foo_val.as_bytes())
+        .arg(&value)
         .query_async(&mut con)
         .await?;
-    let () = redis::cmd("SET")
+
+    redis::cmd("SET")
         .arg(&[&key2, "bar"])
         .query_async(&mut con)
         .await?;
+
     redis::cmd("MGET")
-        .arg(&[&key_2, &key2_2])
+        .arg(&[&key, &key2])
         .query_async(&mut con)
         .map(|result| {
-            assert_eq!(Ok((foo_val, b"bar".to_vec())), result);
+            assert_eq!(Ok((value, b"bar".to_vec())), result);
             Ok(())
         })
         .await
@@ -33,16 +31,12 @@ async fn test_cmd(con: &MultiplexedConnection, i: i32) -> RedisResult<()> {
 
 #[tokio::main]
 async fn main() {
-    println!("Main starting...");
-
     let client = redis::Client::open("redis://127.0.0.1/").unwrap();
 
-    // features = ["tokio-rt-core"] must be specified on redis crate
     let con = client.get_multiplexed_tokio_connection().await.unwrap();
 
     let cmds = (0..100).map(|i| test_cmd(&con, i));
     let result = future::try_join_all(cmds).await.unwrap();
-    assert_eq!(100, result.len());
 
-    println!("Main done.");
+    assert_eq!(100, result.len());
 }
