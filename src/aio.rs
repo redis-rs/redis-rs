11ad44016fa213bc5564f57524661320396d345a
Markus Westerlind <markus.westerlind@distilnetworks.com>
2020-06-23 10:08:15 +0200

    fix: Don't hang in password authentication using a multiplexed connection
    Fixes #343

--- src/aio.rs
+++ src/aio.rs
@@ -822,7 +822,20 @@ impl MultiplexedConnection {
             pipeline,
             db: connection_info.db,
         };
-        authenticate(connection_info, &mut con).await?;
+        let driver = {
+            let auth = authenticate(connection_info, &mut con);
+            futures_util::pin_mut!(auth);
+
+            match futures_util::future::select(auth, driver).await {
+                futures_util::future::Either::Left((result, driver)) => {
+                    result?;
+                    driver
+                }
+                futures_util::future::Either::Right(((), _)) => {
+                    unreachable!("Multiplexed connection driver unexpectedly terminated")
+                }
+            }
+        };
         Ok((con, driver))
     }
 }
