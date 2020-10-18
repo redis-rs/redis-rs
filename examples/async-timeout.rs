c24bff9872c4ad7e459a2b61ab5af869ccadbc20
Shane Lillie <ignignokterr@gmail.com>
2020-10-18 14:23:11 -0700

    Don't loop the example

--- examples/async-timeout.rs
+++ examples/async-timeout.rs
@@ -1,7 +1,7 @@
 use redis::AsyncCommands;
 use tokio::time;
 
-// use SIGTSTP (Ctrl+Z) to stop the redis server in order to induce timeouts
+// use SIGTSTP (Ctrl+Z) to stop the redis server in order to induce a timeout here
 
 #[tokio::main]
 async fn main() -> redis::RedisResult<()> {
@@ -11,12 +11,10 @@ async fn main() -> redis::RedisResult<()> {
         .get_tokio_connection_manager_timeout(time::Duration::from_secs(5))
         .await?;
 
-    loop {
-        match con.set_ex::<_, _, ()>("async-timeout", 1, 30).await {
-            Ok(_) => (),
-            Err(e) => eprintln!("redis error: {}", e),
-        }
-
-        time::delay_for(time::Duration::from_secs(15)).await;
+    match con.set_ex::<_, _, ()>("async-timeout", 1, 30).await {
+        Ok(_) => (),
+        Err(e) => eprintln!("redis error: {}", e),
     }
+
+    Ok(())
 }
