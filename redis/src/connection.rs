6cb4e3d7bb9b5bf8195e6de3f99619963fa37c77
Leo Rowan <LeoRowan3@gmail.com>
2022-04-27 17:17:29 -0400

    Support most redis protocols, cypher suites, and key exchanges

--- redis/src/connection.rs
+++ redis/src/connection.rs
@@ -22,7 +22,7 @@ use std::os::unix::net::UnixStream;
 use native_tls::{TlsConnector, TlsStream};
 
 #[cfg(feature = "rustls")]
-use rustls::{cipher_suite, kx_group, OwnedTrustAnchor, RootCertStore, StreamOwned};
+use rustls::{OwnedTrustAnchor, RootCertStore, StreamOwned};
 #[cfg(feature = "rustls")]
 use std::{convert::TryInto, sync::Arc};
 #[cfg(feature = "rustls")]
@@ -681,10 +681,9 @@ pub(crate) fn create_rustls_config(insecure: bool) -> RedisResult<rustls::Client
     }));
 
     let mut config = rustls::ClientConfig::builder()
-        .with_cipher_suites(&[cipher_suite::TLS13_CHACHA20_POLY1305_SHA256])
-        .with_kx_groups(&[&kx_group::X25519])
-        .with_protocol_versions(&[&rustls::version::TLS13])
-        .unwrap()
+        .with_safe_default_cipher_suites()
+        .with_safe_default_kx_groups()
+        .with_protocol_versions(rustls::ALL_VERSIONS)?
         .with_root_certificates(root_store)
         .with_no_client_auth();
 
