82eeb45ac71a2c178af5088b913469f9597595f4
Gibran Amparan <rafael.amparan@agileengine.com>
2022-12-11 10:37:37 -0600

    Add hashmap to redis args (#722)

--- redis/tests/test_types.rs
+++ redis/tests/test_types.rs
@@ -230,6 +230,7 @@ fn test_types_to_redis_args() {
     use redis::ToRedisArgs;
     use std::collections::BTreeMap;
     use std::collections::BTreeSet;
+    use std::collections::HashMap;
     use std::collections::HashSet;
 
     assert!(!5i32.to_redis_args().is_empty());
@@ -256,6 +257,14 @@ fn test_types_to_redis_args() {
         .iter()
         .cloned()
         .collect::<BTreeMap<_, _>>()
+        .to_redis_args()
+        .is_empty());
+
+    // this can also be used on something HMSET
+    assert!(![("d", 8), ("e", 9), ("f", 10)]
+        .iter()
+        .cloned()
+        .collect::<HashMap<_, _>>()
         .to_redis_args()
         .is_empty());
 }
