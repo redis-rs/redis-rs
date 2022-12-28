14bcdd34e61c09634046cc31ad236e9104977545
Joe Chen <geometryolife@gmail.com>
2022-12-28 20:47:50 +0800

    fix typo in `redis/src/lib.rs`

--- redis/src/lib.rs
+++ redis/src/lib.rs
@@ -18,7 +18,7 @@
 //!
 //! # Basic Operation
 //!
-//! redis-rs exposes two API levels: a low- and a high-level part.
+//! redis-rs exposes two API levels: a low-level and a high-level part.
 //! The high-level part does not expose all the functionality of redis and
 //! might take some liberties in how it speaks the protocol.  The low-level
 //! part of the API allows you to express any request on the redis level.
