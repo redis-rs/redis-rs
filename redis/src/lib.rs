00c2620f6ac1749382f1029239bd7a09f4c20d4f
nonirosenfeldredis <sharon.rosenfeld@redis.com>
2023-02-23 17:33:06 +0200

    passed all tests

--- redis/src/lib.rs
+++ redis/src/lib.rs
@@ -452,6 +452,10 @@ pub mod streams;
 #[cfg_attr(docsrs, doc(cfg(feature = "tls")))]
 pub mod tls;
 
+#[cfg(feature = "tls")]
+#[cfg_attr(docsrs, doc(cfg(feature = "tls")))]
+pub mod tls;
+
 mod client;
 mod cmd;
 mod commands;
