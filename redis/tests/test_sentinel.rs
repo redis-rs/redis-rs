43533858e9e8e0d159778c67f7d5cb23ab72da2c
Felipe Machado <462154+felipou@users.noreply.github.com>
2023-05-20 11:01:24 -0300

    Update sentinel tests to be more robust
    Instead of using get/set, which depends on the actual replication of
    the data, we're only interested here in knowing that we got the correct
    clients, so now we just check the state of the servers - check the
    role (master/replica) and for replicas we check the master's address.

--- redis/tests/test_sentinel.rs
+++ redis/tests/test_sentinel.rs
@@ -1,8 +1,53 @@
 mod support;
 
-use redis::sentinel::SentinelClient;
+use std::collections::HashMap;
+
+use redis::{sentinel::SentinelClient, Client, Connection, ConnectionAddr};
 
 use crate::support::*;
+
+fn parse_replication_info(value: &str) -> HashMap<&str, &str> {
+    let info_map: std::collections::HashMap<&str, &str> = value
+        .split("\r\n")
+        .filter(|line| !line.trim_start().starts_with('#'))
+        .filter_map(|line| line.split_once(':'))
+        .collect();
+    info_map
+}
+
+fn check_master_role(conn: &mut Connection) {
+    let info: String = redis::cmd("INFO").arg("REPLICATION").query(conn).unwrap();
+
+    let info_map = parse_replication_info(&info);
+    assert_eq!(info_map.get("role"), Some(&"master"));
+}
+
+fn check_replica_info_result(info: String, master_client: &Client) {
+    let info_map = parse_replication_info(&info);
+
+    assert_eq!(info_map.get("role"), Some(&"slave"));
+
+    let (master_host, master_port) = match &master_client.get_connection_info().addr {
+        ConnectionAddr::Tcp(host, port) => (host, port),
+        ConnectionAddr::TcpTls {
+            host,
+            port,
+            insecure: _,
+        } => (host, port),
+        ConnectionAddr::Unix(..) => panic!("Unexpected master connection type"),
+    };
+
+    assert_eq!(info_map.get("master_host"), Some(&master_host.as_str()));
+    assert_eq!(
+        info_map.get("master_port"),
+        Some(&master_port.to_string().as_str())
+    );
+}
+
+fn check_replica_role_and_master(conn: &mut Connection, master_client: &Client) {
+    let info: String = redis::cmd("INFO").arg("REPLICATION").query(conn).unwrap();
+    check_replica_info_result(info, master_client);
+}
 
 #[test]
 fn test_sentinel_basics() {
@@ -15,20 +60,7 @@ fn test_sentinel_basics() {
         .get_connection()
         .unwrap();
 
-    redis::cmd("SET")
-        .arg("{x}key1")
-        .arg(b"foo")
-        .execute(&mut master_con);
-    redis::cmd("SET")
-        .arg(&["{x}key2", "bar"])
-        .execute(&mut master_con);
-
-    assert_eq!(
-        redis::cmd("MGET")
-            .arg(&["{x}key1", "{x}key2"])
-            .query(&mut master_con),
-        Ok(("foo".to_string(), b"bar".to_vec()))
-    );
+    check_master_role(&mut master_con);
 }
 
 #[test]
@@ -36,11 +68,8 @@ fn test_sentinel_read_from_random_replica() {
     let cluster = TestSentinelContext::new(3, 1, 3);
     let sentinel = cluster.sentinel();
 
-    let mut master_con = sentinel
-        .master_for("master1")
-        .unwrap()
-        .get_connection()
-        .unwrap();
+    let master_client = sentinel.master_for("master1").unwrap();
+    let mut master_con = master_client.get_connection().unwrap();
 
     let mut replica_con = sentinel
         .replica_for("master1")
@@ -48,22 +77,8 @@ fn test_sentinel_read_from_random_replica() {
         .get_connection()
         .unwrap();
 
-    // Write commands would go to the primary nodes
-    redis::cmd("SET")
-        .arg("{x}key1")
-        .arg(b"foo")
-        .execute(&mut master_con);
-    redis::cmd("SET")
-        .arg(&["{x}key2", "bar"])
-        .execute(&mut master_con);
-
-    // Read commands would go to the replica nodes
-    assert_eq!(
-        redis::cmd("MGET")
-            .arg(&["{x}key1", "{x}key2"])
-            .query(&mut replica_con),
-        Ok(("foo".to_string(), b"bar".to_vec()))
-    );
+    check_master_role(&mut master_con);
+    check_replica_role_and_master(&mut replica_con, &master_client);
 }
 
 #[test]
@@ -71,20 +86,10 @@ fn test_sentinel_read_from_multiple_replicas() {
     let mut cluster = TestSentinelContext::new(3, 1, 3);
     let sentinel = cluster.sentinel_mut();
 
-    let mut master_con = sentinel
-        .master_for("master1")
-        .unwrap()
-        .get_connection()
-        .unwrap();
+    let master_client = sentinel.master_for("master1").unwrap();
+    let mut master_con = master_client.get_connection().unwrap();
 
-    // Write commands would go to the primary nodes
-    redis::cmd("SET")
-        .arg("{x}key1")
-        .arg(b"foo")
-        .execute(&mut master_con);
-    redis::cmd("SET")
-        .arg(&["{x}key2", "bar"])
-        .execute(&mut master_con);
+    check_master_role(&mut master_con);
 
     for _ in 0..20 {
         let mut replica_con = sentinel
@@ -93,12 +98,7 @@ fn test_sentinel_read_from_multiple_replicas() {
             .get_connection()
             .unwrap();
 
-        assert_eq!(
-            redis::cmd("MGET")
-                .arg(&["{x}key1", "{x}key2"])
-                .query(&mut replica_con),
-            Ok(("foo".to_string(), b"bar".to_vec()))
-        );
+        check_replica_role_and_master(&mut replica_con, &master_client);
     }
 }
 
@@ -107,20 +107,10 @@ fn test_sentinel_server_down() {
     let mut context = TestSentinelContext::new(3, 1, 3);
     let sentinel = context.sentinel_mut();
 
-    let mut master_con = sentinel
-        .master_for("master1")
-        .unwrap()
-        .get_connection()
-        .unwrap();
+    let master_client = sentinel.master_for("master1").unwrap();
+    let mut master_con = master_client.get_connection().unwrap();
 
-    // Write commands would go to the primary nodes
-    redis::cmd("SET")
-        .arg("{x}key1")
-        .arg(b"foo")
-        .execute(&mut master_con);
-    redis::cmd("SET")
-        .arg(&["{x}key2", "bar"])
-        .execute(&mut master_con);
+    check_master_role(&mut master_con);
 
     context.cluster.sentinel_servers[0].stop();
     std::thread::sleep(std::time::Duration::from_millis(25));
@@ -134,18 +124,13 @@ fn test_sentinel_server_down() {
             .get_connection()
             .unwrap();
 
-        assert_eq!(
-            redis::cmd("MGET")
-                .arg(&["{x}key1", "{x}key2"])
-                .query(&mut replica_con),
-            Ok(("foo".to_string(), b"bar".to_vec()))
-        );
+        check_replica_role_and_master(&mut replica_con, &master_client);
     }
 }
 
 #[test]
 fn test_sentinel_client() {
-    let context = TestSentinelContext::new(3, 1, 3);
+    let mut context = TestSentinelContext::new(3, 1, 3);
     let master_client = SentinelClient::build(
         context.sentinels_connection_info().clone(),
         String::from("master1"),
@@ -162,32 +147,44 @@ fn test_sentinel_client() {
 
     let mut master_con = master_client.get_connection().unwrap();
 
-    // Write commands would go to the primary nodes
-    redis::cmd("SET")
-        .arg("{x}key1")
-        .arg(b"foo")
-        .execute(&mut master_con);
-    redis::cmd("SET")
-        .arg(&["{x}key2", "bar"])
-        .execute(&mut master_con);
+    check_master_role(&mut master_con);
+
+    let sentinel = context.sentinel_mut();
+    let master_client = sentinel.master_for("master1").unwrap();
 
     for _ in 0..20 {
         let mut replica_con = replica_client.get_connection().unwrap();
 
-        assert_eq!(
-            redis::cmd("MGET")
-                .arg(&["{x}key1", "{x}key2"])
-                .query(&mut replica_con),
-            Ok(("foo".to_string(), b"bar".to_vec()))
-        );
+        check_replica_role_and_master(&mut replica_con, &master_client);
     }
 }
 
 #[cfg(feature = "aio")]
 pub mod async_tests {
-    use redis::{sentinel::SentinelClient, RedisError, RedisResult};
+    use redis::{aio::Connection, sentinel::SentinelClient, Client, RedisError};
 
-    use crate::support::*;
+    use crate::{check_replica_info_result, parse_replication_info, support::*};
+
+    async fn async_check_master_role(conn: &mut Connection) {
+        let info: String = redis::cmd("INFO")
+            .arg("REPLICATION")
+            .query_async(conn)
+            .await
+            .unwrap();
+
+        let info_map = parse_replication_info(&info);
+        assert_eq!(info_map.get("role"), Some(&"master"));
+    }
+
+    async fn async_check_replica_role_and_master(conn: &mut Connection, master_client: &Client) {
+        let info: String = redis::cmd("INFO")
+            .arg("REPLICATION")
+            .query_async(conn)
+            .await
+            .unwrap();
+
+        check_replica_info_result(info, master_client);
+    }
 
     #[test]
     fn test_sentinel_basics_async() {
@@ -201,21 +198,7 @@ pub mod async_tests {
                 .get_async_connection()
                 .await?;
 
-            redis::cmd("SET")
-                .arg("{x}key1")
-                .arg(b"foo")
-                .query_async(&mut master_con)
-                .await?;
-            redis::cmd("SET")
-                .arg(&["{x}key2", "bar"])
-                .query_async(&mut master_con)
-                .await?;
-
-            let mget_result: RedisResult<(String, Vec<u8>)> = redis::cmd("MGET")
-                .arg(&["{x}key1", "{x}key2"])
-                .query_async(&mut master_con)
-                .await;
-            assert_eq!(mget_result, Ok(("foo".to_string(), b"bar".to_vec())));
+            async_check_master_role(&mut master_con).await;
 
             Ok::<(), RedisError>(())
         })
@@ -228,11 +211,8 @@ pub mod async_tests {
         let sentinel = cluster.sentinel();
 
         block_on_all(async move {
-            let mut master_con = sentinel
-                .async_master_for("master1")
-                .await?
-                .get_async_connection()
-                .await?;
+            let master_client = sentinel.async_master_for("master1").await?;
+            let mut master_con = master_client.get_async_connection().await?;
 
             let mut replica_con = sentinel
                 .async_replica_for("master1")
@@ -240,23 +220,8 @@ pub mod async_tests {
                 .get_async_connection()
                 .await?;
 
-            // Write commands would go to the primary nodes
-            redis::cmd("SET")
-                .arg("{x}key1")
-                .arg(b"foo")
-                .query_async(&mut master_con)
-                .await?;
-            redis::cmd("SET")
-                .arg(&["{x}key2", "bar"])
-                .query_async(&mut master_con)
-                .await?;
-
-            // Read commands to the replica node
-            let mget_result: RedisResult<(String, Vec<u8>)> = redis::cmd("MGET")
-                .arg(&["{x}key1", "{x}key2"])
-                .query_async(&mut replica_con)
-                .await;
-            assert_eq!(mget_result, Ok(("foo".to_string(), b"bar".to_vec())));
+            async_check_master_role(&mut master_con).await;
+            async_check_replica_role_and_master(&mut replica_con, &master_client).await;
 
             Ok::<(), RedisError>(())
         })
@@ -269,22 +234,10 @@ pub mod async_tests {
         let sentinel = cluster.sentinel_mut();
 
         block_on_all(async move {
-            let mut master_con = sentinel
-                .async_master_for("master1")
-                .await?
-                .get_async_connection()
-                .await?;
+            let master_client = sentinel.async_master_for("master1").await?;
+            let mut master_con = master_client.get_async_connection().await?;
 
-            // Write commands would go to the primary nodes
-            redis::cmd("SET")
-                .arg("{x}key1")
-                .arg(b"foo")
-                .query_async(&mut master_con)
-                .await?;
-            redis::cmd("SET")
-                .arg(&["{x}key2", "bar"])
-                .query_async(&mut master_con)
-                .await?;
+            async_check_master_role(&mut master_con).await;
 
             // Read commands to the replica node
             for _ in 0..20 {
@@ -294,11 +247,7 @@ pub mod async_tests {
                     .get_async_connection()
                     .await?;
 
-                let mget_result: RedisResult<(String, Vec<u8>)> = redis::cmd("MGET")
-                    .arg(&["{x}key1", "{x}key2"])
-                    .query_async(&mut replica_con)
-                    .await;
-                assert_eq!(mget_result, Ok(("foo".to_string(), b"bar".to_vec())));
+                async_check_replica_role_and_master(&mut replica_con, &master_client).await;
             }
 
             Ok::<(), RedisError>(())
@@ -313,22 +262,10 @@ pub mod async_tests {
         block_on_all(async move {
             let sentinel = context.sentinel_mut();
 
-            let mut master_con = sentinel
-                .async_master_for("master1")
-                .await?
-                .get_async_connection()
-                .await?;
+            let master_client = sentinel.async_master_for("master1").await?;
+            let mut master_con = master_client.get_async_connection().await?;
 
-            // Write commands would go to the primary nodes
-            redis::cmd("SET")
-                .arg("{x}key1")
-                .arg(b"foo")
-                .query_async(&mut master_con)
-                .await?;
-            redis::cmd("SET")
-                .arg(&["{x}key2", "bar"])
-                .query_async(&mut master_con)
-                .await?;
+            async_check_master_role(&mut master_con).await;
 
             context.cluster.sentinel_servers[0].stop();
             std::thread::sleep(std::time::Duration::from_millis(25));
@@ -343,11 +280,7 @@ pub mod async_tests {
                     .get_async_connection()
                     .await?;
 
-                let mget_result: RedisResult<(String, Vec<u8>)> = redis::cmd("MGET")
-                    .arg(&["{x}key1", "{x}key2"])
-                    .query_async(&mut replica_con)
-                    .await;
-                assert_eq!(mget_result, Ok(("foo".to_string(), b"bar".to_vec())));
+                async_check_replica_role_and_master(&mut replica_con, &master_client).await;
             }
 
             Ok::<(), RedisError>(())
@@ -357,7 +290,7 @@ pub mod async_tests {
 
     #[test]
     fn test_sentinel_client_async() {
-        let context = TestSentinelContext::new(3, 1, 3);
+        let mut context = TestSentinelContext::new(3, 1, 3);
         let master_client = SentinelClient::build(
             context.sentinels_connection_info().clone(),
             String::from("master1"),
@@ -375,26 +308,16 @@ pub mod async_tests {
         block_on_all(async move {
             let mut master_con = master_client.get_async_connection().await?;
 
-            // Write commands would go to the primary nodes
-            redis::cmd("SET")
-                .arg("{x}key1")
-                .arg(b"foo")
-                .query_async(&mut master_con)
-                .await?;
-            redis::cmd("SET")
-                .arg(&["{x}key2", "bar"])
-                .query_async(&mut master_con)
-                .await?;
+            async_check_master_role(&mut master_con).await;
+
+            let sentinel = context.sentinel_mut();
+            let master_client = sentinel.async_master_for("master1").await?;
 
             // Read commands to the replica node
             for _ in 0..20 {
                 let mut replica_con = replica_client.get_async_connection().await?;
 
-                let mget_result: RedisResult<(String, Vec<u8>)> = redis::cmd("MGET")
-                    .arg(&["{x}key1", "{x}key2"])
-                    .query_async(&mut replica_con)
-                    .await;
-                assert_eq!(mget_result, Ok(("foo".to_string(), b"bar".to_vec())));
+                async_check_replica_role_and_master(&mut replica_con, &master_client).await;
             }
 
             Ok::<(), RedisError>(())
