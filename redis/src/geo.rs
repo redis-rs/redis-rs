a555c44717eb8ff5febdc2c2ca7c62bbc6dc3a64
Shachar Langbeheim <nihohit@gmail.com>
2023-08-11 04:46:42 +0000

    Fix lints.

--- redis/src/geo.rs
+++ redis/src/geo.rs
@@ -104,8 +104,10 @@ impl<T: ToRedisArgs> ToRedisArgs for Coord<T> {
 ///
 /// [1]: https://redis.io/commands/georadius
 /// [2]: https://redis.io/commands/georadiusbymember
+#[derive(Default)]
 pub enum RadiusOrder {
     /// Don't sort the results
+    #[default]
     Unsorted,
 
     /// Sort returned items from the nearest to the farthest, relative to the center.
@@ -113,12 +115,6 @@ pub enum RadiusOrder {
 
     /// Sort returned items from the farthest to the nearest, relative to the center.
     Desc,
-}
-
-impl Default for RadiusOrder {
-    fn default() -> RadiusOrder {
-        RadiusOrder::Unsorted
-    }
 }
 
 /// Options for the [GEORADIUS][1] and [GEORADIUSBYMEMBER][2] commands
