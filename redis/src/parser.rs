a1cc436a2cf07507850410349375d918d6e4fba3
enzious <enzi@braindead.io>
2023-09-01 08:15:19 -0500

    Consume line and return error on unknown parser input

--- redis/src/parser.rs
+++ redis/src/parser.rs
@@ -154,13 +154,23 @@ where
                     })
                 };
 
+                let unknown = || {
+                    line().map(|line| {
+                        RedisError::from((
+                            ErrorKind::ResponseError,
+                            "Unknown response",
+                            format!("Unknown line: {}", line),
+                        ))
+                    })
+                };
+
                 combine::dispatch!(b;
                     b'+' => status().map(Ok),
                     b':' => int().map(|i| Ok(Value::Int(i))),
                     b'$' => data().map(Ok),
                     b'*' => bulk(),
                     b'-' => error().map(Err),
-                    b => combine::unexpected_any(combine::error::Token(b))
+                    _ => unknown().map(Err)
                 )
             })
     ))
