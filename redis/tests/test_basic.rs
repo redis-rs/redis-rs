a08aba18f1b2ac8ebf50e7328a271761f0048c6a
Gheorghita Mutu <gheorghitamutu@gmail.com>
2023-09-25 21:08:01 +0300

    Tests: fix blocking sorted set apis test

--- redis/tests/test_basic.rs
+++ redis/tests/test_basic.rs
@@ -1290,22 +1290,22 @@ fn test_blocking_sorted_set_api() {
     assert_eq!(con.zadd("b", "2b", 2), Ok(()));
     assert_eq!(con.zadd("c", "3c", 3), Ok(()));
     assert_eq!(con.zadd("d", "4d", 4), Ok(()));
-    assert_eq!(con.zadd("a", "5a", 1), Ok(()));
-    assert_eq!(con.zadd("b", "6b", 2), Ok(()));
-    assert_eq!(con.zadd("c", "7c", 3), Ok(()));
-    assert_eq!(con.zadd("d", "8d", 4), Ok(()));
+    assert_eq!(con.zadd("a", "5a", 5), Ok(()));
+    assert_eq!(con.zadd("b", "6b", 6), Ok(()));
+    assert_eq!(con.zadd("c", "7c", 7), Ok(()));
+    assert_eq!(con.zadd("d", "8d", 8), Ok(()));
 
     let result_multiple_min = con
         .bzmpop_min::<&str, (String, Vec<Vec<(String, String)>>)>(0, vec!["a", "b", "c", "d"].as_slice(), 1);
     let result_multiple_max  = con
-        .bzmpop_min::<&str, (String, Vec<Vec<(String, String)>>)>(0, vec!["a", "b", "c", "d"].as_slice(), 1);
+        .bzmpop_max::<&str, (String, Vec<Vec<(String, String)>>)>(0, vec!["a", "b", "c", "d"].as_slice(), 1);
 
     let result_min = con.bzpopmin::<&str, (String, String, String)>("b", 0);
-    let result_max = con.bzpopmin::<&str, (String, String, String)>("b", 0);
+    let result_max = con.bzpopmax::<&str, (String, String, String)>("b", 0);
 
     assert_eq!(result_multiple_min.unwrap().1[0][0], (String::from("1a"), String::from("1")));
-    assert_eq!(result_multiple_max.unwrap().1[0][0], (String::from("5a"), String::from("1")));
+    assert_eq!(result_multiple_max.unwrap().1[0][0], (String::from("5a"), String::from("5")));
 
     assert_eq!(result_min.unwrap(), (String::from("b"), String::from("2b"), String::from("2")));
-    assert_eq!(result_max.unwrap(), (String::from("b"), String::from("6b"), String::from("2")));
+    assert_eq!(result_max.unwrap(), (String::from("b"), String::from("6b"), String::from("6")));
 }
