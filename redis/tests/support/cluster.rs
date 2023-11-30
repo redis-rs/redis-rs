de2e0969730bdc05b36a84ad2d4ee147066aaade
Altan Özlü <5479094+altanozlu@users.noreply.github.com>
2023-11-30 12:52:51 +0300

    apply changes from #994

--- redis/tests/support/cluster.rs
+++ redis/tests/support/cluster.rs
@@ -92,6 +92,8 @@ impl RedisCluster {
             is_tls = true;
         }
 
+        let max_attempts = 5;
+
         for node in 0..nodes {
             let port = start_port + node;
 
@@ -130,7 +132,30 @@ impl RedisCluster {
                     cmd.current_dir(tempdir.path());
                     folders.push(tempdir);
                     addrs.push(format!("127.0.0.1:{port}"));
-                    cmd.spawn().unwrap()
+
+                    let mut cur_attempts = 0;
+                    loop {
+                        let mut process = cmd.spawn().unwrap();
+                        sleep(Duration::from_millis(100));
+
+                        match process.try_wait() {
+                            Ok(Some(status)) => {
+                                let err =
+                                    format!("redis server creation failed with status {status:?}");
+                                if cur_attempts == max_attempts {
+                                    panic!("{err}");
+                                }
+                                eprintln!("Retrying: {err}");
+                                cur_attempts += 1;
+                            }
+                            Ok(None) => {
+                                return process;
+                            }
+                            Err(e) => {
+                                panic!("Unexpected error in redis server creation {e}");
+                            }
+                        }
+                    }
                 },
             ));
         }
@@ -149,8 +174,22 @@ impl RedisCluster {
         if is_tls {
             cmd.arg("--tls").arg("--insecure");
         }
-        let status = cmd.status().unwrap();
-        assert!(status.success());
+
+        let mut cur_attempts = 0;
+        loop {
+            let output = cmd.output().unwrap();
+            if output.status.success() {
+                break;
+            } else {
+                let err = format!("Cluster creation failed: {output:?}");
+                if cur_attempts == max_attempts {
+                    panic!("{err}");
+                }
+                eprintln!("Retrying: {err}");
+                sleep(Duration::from_millis(50));
+                cur_attempts += 1;
+            }
+        }
 
         let cluster = RedisCluster { servers, folders };
         if replicas > 0 {
