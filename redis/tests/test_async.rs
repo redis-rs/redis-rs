redis-rs/redis-rs

commit 70dfb95713fbcfe944a9262189c2adf23c12977c

MultiplexedConnection: Remove unnecessary allocation in send. (#990)
* Remove unnecessary generics in mlutiplexer.

* Remove unnecessary allocation in send.

Instead of using a Vec<Value> and allocating / popping values from it,
single requests will now create only a single Value object, which will
reduce the heap usage for single requests.

redis/src/aio/multiplexed_connection.rs
@@ -26,32 +26,32 @@ use std::task::{self, Poll};
 use tokio_util::codec::Decoder;
 
 // Senders which the result of a single request are sent through
-type PipelineOutput<O> = oneshot::Sender<Result<Vec<O>, RedisError>>;
+type PipelineOutput = oneshot::Sender<RedisResult<Value>>;
 
-struct InFlight<O> {
-    output: PipelineOutput<O>,
+struct InFlight {
+    output: PipelineOutput,
     expected_response_count: usize,
     current_response_count: usize,
-    buffer: Vec<O>,
+    buffer: Option<Value>,
     first_err: Option<RedisError>,
 }
 
-impl<O> InFlight<O> {
-    fn new(output: PipelineOutput<O>, expected_response_count: usize) -> Self {
+impl InFlight {
+    fn new(output: PipelineOutput, expected_response_count: usize) -> Self {
         Self {
             output,
             expected_response_count,
             current_response_count: 0,
-            buffer: Vec::new(),
+            buffer: None,
             first_err: None,
         }
     }
 }
 
 // A single message sent through the pipeline
-struct PipelineMessage<S, I> {
+struct PipelineMessage<S> {
     input: S,
-    output: PipelineOutput<I>,
+    output: PipelineOutput,
     response_count: usize,
 }
 
@@ -59,18 +59,17 @@ struct PipelineMessage<S, I> {
 /// items being output by the `Stream` (the number is specified at time of sending). With the
 /// interface provided by `Pipeline` an easy interface of request to response, hiding the `Stream`
 /// and `Sink`.
-struct Pipeline<SinkItem, I>(mpsc::Sender<PipelineMessage<SinkItem, I>>);
+struct Pipeline<SinkItem>(mpsc::Sender<PipelineMessage<SinkItem>>);
 
-impl<SinkItem, I> Clone for Pipeline<SinkItem, I> {
+impl<SinkItem> Clone for Pipeline<SinkItem> {
     fn clone(&self) -> Self {
         Pipeline(self.0.clone())
     }
 }
 
-impl<SinkItem, I> Debug for Pipeline<SinkItem, I>
+impl<SinkItem> Debug for Pipeline<SinkItem>
 where
     SinkItem: Debug,
-    I: Debug,
 {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.debug_tuple("Pipeline").field(&self.0).finish()
@@ -78,21 +77,21 @@ where
 }
 
 pin_project! {
-    struct PipelineSink<T, I> {
+    struct PipelineSink<T> {
         #[pin]
         sink_stream: T,
-        in_flight: VecDeque<InFlight<I>>,
+        in_flight: VecDeque<InFlight>,
         error: Option<RedisError>,
     }
 }
 
-impl<T, I> PipelineSink<T, I>
+impl<T> PipelineSink<T>
 where
-    T: Stream<Item = Result<I, RedisError>> + 'static,
+    T: Stream<Item = RedisResult<Value>> + 'static,
 {
     fn new<SinkItem>(sink_stream: T) -> Self
     where
-        T: Sink<SinkItem, Error = RedisError> + Stream<Item = Result<I, RedisError>> + 'static,
+        T: Sink<SinkItem, Error = RedisError> + Stream<Item = RedisResult<Value>> + 'static,
     {
         PipelineSink {
             sink_stream,
@@ -118,7 +117,7 @@ where
         }
     }
 
-    fn send_result(self: Pin<&mut Self>, result: Result<I, RedisError>) {
+    fn send_result(self: Pin<&mut Self>, result: RedisResult<Value>) {
         let self_ = self.project();
 
         {
@@ -129,7 +128,19 @@ where
 
             match result {
                 Ok(item) => {
-                    entry.buffer.push(item);
+                    entry.buffer = Some(match entry.buffer.take() {
+                        Some(Value::Bulk(mut values)) => {
+                            values.push(item);
+                            Value::Bulk(values)
+                        }
+                        Some(value) => {
+                            let mut vec = Vec::with_capacity(entry.expected_response_count);
+                            vec.push(value);
+                            vec.push(item);
+                            Value::Bulk(vec)
+                        }
+                        None => item,
+                    });
                 }
                 Err(err) => {
                     if entry.first_err.is_none() {
@@ -148,7 +159,7 @@ where
         let entry = self_.in_flight.pop_front().unwrap();
         let response = match entry.first_err {
             Some(err) => Err(err),
-            None => Ok(entry.buffer),
+            None => Ok(entry.buffer.unwrap_or(Value::Bulk(vec![]))),
         };
 
         // `Err` means that the receiver was dropped in which case it does not
@@ -158,9 +169,9 @@ where
     }
 }
 
-impl<SinkItem, T, I> Sink<PipelineMessage<SinkItem, I>> for PipelineSink<T, I>
+impl<SinkItem, T> Sink<PipelineMessage<SinkItem>> for PipelineSink<T>
 where
-    T: Sink<SinkItem, Error = RedisError> + Stream<Item = Result<I, RedisError>> + 'static,
+    T: Sink<SinkItem, Error = RedisError> + Stream<Item = RedisResult<Value>> + 'static,
 {
     type Error = ();
 
@@ -184,7 +195,7 @@ where
             input,
             output,
             response_count,
-        }: PipelineMessage<SinkItem, I>,
+        }: PipelineMessage<SinkItem>,
     ) -> Result<(), Self::Error> {
         // If there is nothing to receive our output we do not need to send the message as it is
         // ambiguous whether the message will be sent anyway. Helps shed some load on the
@@ -245,14 +256,13 @@ where
     }
 }
 
-impl<SinkItem, I> Pipeline<SinkItem, I>
+impl<SinkItem> Pipeline<SinkItem>
 where
     SinkItem: Send + 'static,
-    I: Send + 'static,
 {
     fn new<T>(sink_stream: T) -> (Self, impl Future<Output = ()>)
     where
-        T: Sink<SinkItem, Error = RedisError> + Stream<Item = Result<I, RedisError>> + 'static,
+        T: Sink<SinkItem, Error = RedisError> + Stream<Item = RedisResult<Value>> + 'static,
         T: Send + 'static,
         T::Item: Send,
         T::Error: Send,
@@ -268,23 +278,20 @@ where
     }
 
     // `None` means that the stream was out of items causing that poll loop to shut down.
-    async fn send(
+    async fn send_single(
         &mut self,
         item: SinkItem,
         timeout: futures_time::time::Duration,
-    ) -> Result<I, Option<RedisError>> {
-        self.send_recv_multiple(item, 1, timeout)
-            .await
-            // We can unwrap since we do a request for `1` item
-            .map(|mut item| item.pop().unwrap())
+    ) -> Result<Value, Option<RedisError>> {
+        self.send_recv(item, 1, timeout).await
     }
 
-    async fn send_recv_multiple(
+    async fn send_recv(
         &mut self,
         input: SinkItem,
         count: usize,
         timeout: futures_time::time::Duration,
-    ) -> Result<Vec<I>, Option<RedisError>> {
+    ) -> Result<Value, Option<RedisError>> {
         let (sender, receiver) = oneshot::channel();
 
         self.0
@@ -311,7 +318,7 @@ where
 /// on the same underlying connection (tcp/unix socket).
 #[derive(Clone)]
 pub struct MultiplexedConnection {
-    pipeline: Pipeline<Vec<u8>, Value>,
+    pipeline: Pipeline<Vec<u8>>,
     db: i64,
     response_timeout: futures_time::time::Duration,
 }
@@ -393,7 +400,7 @@ impl MultiplexedConnection {
     /// reads the single response from it.
     pub async fn send_packed_command(&mut self, cmd: &Cmd) -> RedisResult<Value> {
         self.pipeline
-            .send(cmd.get_packed_command(), self.response_timeout)
+            .send_single(cmd.get_packed_command(), self.response_timeout)
             .await
             .map_err(|err| {
                 err.unwrap_or_else(|| RedisError::from(io::Error::from(io::ErrorKind::BrokenPipe)))
@@ -409,9 +416,9 @@ impl MultiplexedConnection {
         offset: usize,
         count: usize,
     ) -> RedisResult<Vec<Value>> {
-        let mut value = self
+        let value = self
             .pipeline
-            .send_recv_multiple(
+            .send_recv(
                 cmd.get_packed_pipeline(),
                 offset + count,
                 self.response_timeout,
@@ -421,8 +428,13 @@ impl MultiplexedConnection {
                 err.unwrap_or_else(|| RedisError::from(io::Error::from(io::ErrorKind::BrokenPipe)))
             })?;
 
-        value.drain(..offset);
-        Ok(value)
+        match value {
+            Value::Bulk(mut values) => {
+                values.drain(..offset);
+                Ok(values)
+            }
+            _ => Ok(vec![value]),
+        }
     }
 }
 

commit a205455e64fd9ebea25459986671a48a8411959a

MultiplexedConnection: Separate response handling for pipeline.
This fixes a bug where pipeline responses for a single request that return a `Vec` as that single value aren't wrapped in another `Vec`.
Since this is the second bug introduced by https://github.com/redis-rs/redis-rs/commit/70dfb95713fbcfe944a9262189c2adf23c12977c,
while we still want to maintain the performance improvement for single requests introduced by same, the fix is to separate the logic of single responses from pipeline responses.

redis/src/aio/multiplexed_connection.rs
@@ -29,31 +29,41 @@ use tokio_util::codec::Decoder;
 // Senders which the result of a single request are sent through
 type PipelineOutput = oneshot::Sender<RedisResult<Value>>;
 
-struct InFlight {
-    output: PipelineOutput,
-    expected_response_count: usize,
-    current_response_count: usize,
-    buffer: Option<Value>,
-    first_err: Option<RedisError>,
+enum ResponseAggregate {
+    SingleCommand,
+    Pipeline {
+        expected_response_count: usize,
+        current_response_count: usize,
+        buffer: Vec<Value>,
+        first_err: Option<RedisError>,
+    },
 }
 
-impl InFlight {
-    fn new(output: PipelineOutput, expected_response_count: usize) -> Self {
-        Self {
-            output,
-            expected_response_count,
-            current_response_count: 0,
-            buffer: None,
-            first_err: None,
+impl ResponseAggregate {
+    fn new(pipeline_response_count: Option<usize>) -> Self {
+        match pipeline_response_count {
+            Some(response_count) => ResponseAggregate::Pipeline {
+                expected_response_count: response_count,
+                current_response_count: 0,
+                buffer: Vec::new(),
+                first_err: None,
+            },
+            None => ResponseAggregate::SingleCommand,
         }
     }
+}
+
+struct InFlight {
+    output: PipelineOutput,
+    response_aggregate: ResponseAggregate,
 }
 
 // A single message sent through the pipeline
 struct PipelineMessage<S> {
     input: S,
     output: PipelineOutput,
-    response_count: usize,
+    // If `None`, this is a single request, not a pipeline of multiple requests.
+    pipeline_response_count: Option<usize>,
 }
 
 /// Wrapper around a `Stream + Sink` where each item sent through the `Sink` results in one or more
@@ -122,51 +132,51 @@ where
         let self_ = self.project();
 
         {
-            let entry = match self_.in_flight.front_mut() {
+            let mut entry = match self_.in_flight.pop_front() {
                 Some(entry) => entry,
                 None => return,
             };
 
-            match result {
-                Ok(item) => {
-                    entry.buffer = Some(match entry.buffer.take() {
-                        Some(Value::Bulk(mut values)) if entry.current_response_count > 1 => {
-                            values.push(item);
-                            Value::Bulk(values)
-                        }
-                        Some(value) => {
-                            let mut vec = Vec::with_capacity(entry.expected_response_count);
-                            vec.push(value);
-                            vec.push(item);
-                            Value::Bulk(vec)
-                        }
-                        None => item,
-                    });
+            match &mut entry.response_aggregate {
+                ResponseAggregate::SingleCommand => {
+                    entry.output.send(result).ok();
                 }
-                Err(err) => {
-                    if entry.first_err.is_none() {
-                        entry.first_err = Some(err);
+                ResponseAggregate::Pipeline {
+                    expected_response_count,
+                    current_response_count,
+                    buffer,
+                    first_err,
+                } => {
+                    match result {
+                        Ok(item) => {
+                            buffer.push(item);
+                        }
+                        Err(err) => {
+                            if first_err.is_none() {
+                                *first_err = Some(err);
+                            }
+                        }
                     }
-                }
-            }
 
-            entry.current_response_count += 1;
-            if entry.current_response_count < entry.expected_response_count {
-                // Need to gather more response values
-                return;
+                    *current_response_count += 1;
+                    if current_response_count < expected_response_count {
+                        // Need to gather more response values
+                        self_.in_flight.push_front(entry);
+                        return;
+                    }
+
+                    let response = match first_err.take() {
+                        Some(err) => Err(err),
+                        None => Ok(Value::Bulk(std::mem::take(buffer))),
+                    };
+
+                    // `Err` means that the receiver was dropped in which case it does not
+                    // care about the output and we can continue by just dropping the value
+                    // and sender
+                    entry.output.send(response).ok();
+                }
             }
         }
-
-        let entry = self_.in_flight.pop_front().unwrap();
-        let response = match entry.first_err {
-            Some(err) => Err(err),
-            None => Ok(entry.buffer.unwrap_or(Value::Bulk(vec![]))),
-        };
-
-        // `Err` means that the receiver was dropped in which case it does not
-        // care about the output and we can continue by just dropping the value
-        // and sender
-        entry.output.send(response).ok();
     }
 }
 
@@ -195,7 +205,7 @@ where
         PipelineMessage {
             input,
             output,
-            response_count,
+            pipeline_response_count,
         }: PipelineMessage<SinkItem>,
     ) -> Result<(), Self::Error> {
         // If there is nothing to receive our output we do not need to send the message as it is
@@ -214,9 +224,13 @@ where
 
         match self_.sink_stream.start_send(input) {
             Ok(()) => {
-                self_
-                    .in_flight
-                    .push_back(InFlight::new(output, response_count));
+                let response_aggregate = ResponseAggregate::new(pipeline_response_count);
+                let entry = InFlight {
+                    output,
+                    response_aggregate,
+                };
+
+                self_.in_flight.push_back(entry);
                 Ok(())
             }
             Err(err) => {
@@ -284,13 +298,14 @@ where
         item: SinkItem,
         timeout: Duration,
     ) -> Result<Value, Option<RedisError>> {
-        self.send_recv(item, 1, timeout).await
+        self.send_recv(item, None, timeout).await
     }
 
     async fn send_recv(
         &mut self,
         input: SinkItem,
-        count: usize,
+        // If `None`, this is a single request, not a pipeline of multiple requests.
+        pipeline_response_count: Option<usize>,
         timeout: Duration,
     ) -> Result<Value, Option<RedisError>> {
         let (sender, receiver) = oneshot::channel();
@@ -298,7 +313,7 @@ where
         self.0
             .send(PipelineMessage {
                 input,
-                response_count: count,
+                pipeline_response_count,
                 output: sender,
             })
             .await
@@ -424,7 +439,7 @@ impl MultiplexedConnection {
             .pipeline
             .send_recv(
                 cmd.get_packed_pipeline(),
-                offset + count,
+                Some(offset + count),
                 self.response_timeout,
             )
             .await

redis/tests/test_async.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use futures::{prelude::*, StreamExt};
 use redis::{
     aio::{ConnectionLike, MultiplexedConnection},
@@ -5,7 +7,6 @@ use redis::{
 };
 
 use crate::support::*;
-
 mod support;
 
 #[test]
@@ -30,6 +31,61 @@ fn test_args() {
         assert_eq!(result, Ok(("foo".to_string(), b"bar".to_vec())));
         result
     }))
+    .unwrap();
+}
+
+#[test]
+fn test_nice_hash_api() {
+    let ctx = TestContext::new();
+
+    block_on_all(async move {
+        let mut connection = ctx.async_connection().await.unwrap();
+
+        assert_eq!(
+            connection
+                .hset_multiple("my_hash", &[("f1", 1), ("f2", 2), ("f3", 4), ("f4", 8)])
+                .await,
+            Ok(())
+        );
+
+        let hm: HashMap<String, isize> = connection.hgetall("my_hash").await.unwrap();
+        assert_eq!(hm.len(), 4);
+        assert_eq!(hm.get("f1"), Some(&1));
+        assert_eq!(hm.get("f2"), Some(&2));
+        assert_eq!(hm.get("f3"), Some(&4));
+        assert_eq!(hm.get("f4"), Some(&8));
+        Ok(())
+    })
+    .unwrap();
+}
+
+#[test]
+fn test_nice_hash_api_in_pipe() {
+    let ctx = TestContext::new();
+
+    block_on_all(async move {
+        let mut connection = ctx.async_connection().await.unwrap();
+
+        assert_eq!(
+            connection
+                .hset_multiple("my_hash", &[("f1", 1), ("f2", 2), ("f3", 4), ("f4", 8)])
+                .await,
+            Ok(())
+        );
+
+        let mut pipe = redis::pipe();
+        pipe.cmd("HGETALL").arg("my_hash");
+        let mut vec: Vec<HashMap<String, isize>> = pipe.query_async(&mut connection).await.unwrap();
+        assert_eq!(vec.len(), 1);
+        let hash = vec.pop().unwrap();
+        assert_eq!(hash.len(), 4);
+        assert_eq!(hash.get("f1"), Some(&1));
+        assert_eq!(hash.get("f2"), Some(&2));
+        assert_eq!(hash.get("f3"), Some(&4));
+        assert_eq!(hash.get("f4"), Some(&8));
+
+        Ok(())
+    })
     .unwrap();
 }
 
@@ -542,7 +598,6 @@ async fn test_issue_async_commands_scan_broken() {
 }
 
 mod pub_sub {
-    use std::collections::HashMap;
     use std::time::Duration;
 
     use super::*;