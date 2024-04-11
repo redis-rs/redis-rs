7e79e3a380a07eb0c1e559d9afa9152a87d2e50c
David Herberth <github@dav1d.de>
2024-04-11 08:48:33 +0200

    Add invoke_script to commands to allow for pipelining of scripts

--- redis/src/lib.rs
+++ redis/src/lib.rs
@@ -299,8 +299,7 @@
 # Scripts
 
 Lua scripts are supported through the `Script` type in a convenient
-way (it does not support pipelining currently).  It will automatically
-load the script if it does not exist and invoke it.
+way.  It will automatically load the script if it does not exist and invoke it.
 
 Example:
 
@@ -311,10 +310,33 @@ Example:
 let script = redis::Script::new(r"
     return tonumber(ARGV[1]) + tonumber(ARGV[2]);
 ");
-let result : isize = script.arg(1).arg(2).invoke(&mut con)?;
+let result: isize = script.arg(1).arg(2).invoke(&mut con)?;
 assert_eq!(result, 3);
 # Ok(()) }
 ```
+
+Scripts can also be pipelined:
+
+```rust,no_run
+# fn do_something() -> redis::RedisResult<()> {
+# let client = redis::Client::open("redis://127.0.0.1/").unwrap();
+# let mut con = client.get_connection().unwrap();
+let script = redis::Script::new(r"
+    return tonumber(ARGV[1]) + tonumber(ARGV[2]);
+");
+let (a, b): (isize, isize) = redis::pipe()
+    .invoke_script(script.arg(1).arg(2))
+    .invoke_script(script.arg(2).arg(3))
+    .query(&mut con)?;
+
+assert_eq!(a, 3);
+assert_eq!(b, 5);
+# Ok(()) }
+```
+
+Note: unlike a call to [`invoke`](ScriptInvocation::invoke), if the script isn't loaded during the pipeline operation,
+it will not automatically be loaded and retried. The script can be loaded using the 
+[`load`](ScriptInvocation::load) operation.
 "##
 )]
 //!
--- redis/tests/test_basic.rs
+++ redis/tests/test_basic.rs
@@ -914,41 +914,6 @@ mod basic {
     }
 
     #[test]
-    #[cfg(feature = "script")]
-    fn test_script() {
-        let ctx = TestContext::new();
-        let mut con = ctx.connection();
-
-        let script = redis::Script::new(
-            r"
-       return {redis.call('GET', KEYS[1]), ARGV[1]}
-    ",
-        );
-
-        let _: () = redis::cmd("SET")
-            .arg("my_key")
-            .arg("foo")
-            .query(&mut con)
-            .unwrap();
-        let response = script.key("my_key").arg(42).invoke(&mut con);
-
-        assert_eq!(response, Ok(("foo".to_string(), 42)));
-    }
-
-    #[test]
-    #[cfg(feature = "script")]
-    fn test_script_load() {
-        let ctx = TestContext::new();
-        let mut con = ctx.connection();
-
-        let script = redis::Script::new("return 'Hello World'");
-
-        let hash = script.prepare_invoke().load(&mut con);
-
-        assert_eq!(hash, Ok(script.get_hash().to_string()));
-    }
-
-    #[test]
     fn test_tuple_args() {
         let ctx = TestContext::new();
         let mut con = ctx.connection();
