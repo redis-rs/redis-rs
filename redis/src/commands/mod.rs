5c6db272c9f0cb6e248af738ab0c6f43cae2092c
Shachar Langbeheim <nihohit@gmail.com>
2024-05-07 17:36:08 +0300

    Fix clippy warnings (#1180)

--- redis/src/commands/mod.rs
+++ redis/src/commands/mod.rs
@@ -1,9 +1,10 @@
-// can't use rustfmt here because it screws up the file.
-#![cfg_attr(rustfmt, rustfmt_skip)]
 use crate::cmd::{cmd, Cmd, Iter};
 use crate::connection::{Connection, ConnectionLike, Msg};
 use crate::pipeline::Pipeline;
-use crate::types::{FromRedisValue, NumericBehavior, RedisResult, ToRedisArgs, RedisWrite, Expiry, SetExpiry, ExistenceCheck};
+use crate::types::{
+    ExistenceCheck, Expiry, FromRedisValue, NumericBehavior, RedisResult, RedisWrite, SetExpiry,
+    ToRedisArgs,
+};
 
 #[macro_use]
 mod macros;
@@ -34,7 +35,96 @@ use crate::acl;
 pub(crate) fn is_readonly_cmd(cmd: &[u8]) -> bool {
     matches!(
         cmd,
-        b"BITCOUNT" | b"BITFIELD_RO" | b"BITPOS" | b"DBSIZE" | b"DUMP" | b"EVALSHA_RO" | b"EVAL_RO" | b"EXISTS" | b"EXPIRETIME" | b"FCALL_RO" | b"GEODIST" | b"GEOHASH" | b"GEOPOS" | b"GEORADIUSBYMEMBER_RO" | b"GEORADIUS_RO" | b"GEOSEARCH" | b"GET" | b"GETBIT" | b"GETRANGE" | b"HEXISTS" | b"HGET" | b"HGETALL" | b"HKEYS" | b"HLEN" | b"HMGET" | b"HRANDFIELD" | b"HSCAN" | b"HSTRLEN" | b"HVALS" | b"KEYS" | b"LCS" | b"LINDEX" | b"LLEN" | b"LOLWUT" | b"LPOS" | b"LRANGE" | b"MEMORY USAGE" | b"MGET" | b"OBJECT ENCODING" | b"OBJECT FREQ" | b"OBJECT IDLETIME" | b"OBJECT REFCOUNT" | b"PEXPIRETIME" | b"PFCOUNT" | b"PTTL" | b"RANDOMKEY" | b"SCAN" | b"SCARD" | b"SDIFF" | b"SINTER" | b"SINTERCARD" | b"SISMEMBER" | b"SMEMBERS" | b"SMISMEMBER" | b"SORT_RO" | b"SRANDMEMBER" | b"SSCAN" | b"STRLEN" | b"SUBSTR" | b"SUNION" | b"TOUCH" | b"TTL" | b"TYPE" | b"XINFO CONSUMERS" | b"XINFO GROUPS" | b"XINFO STREAM" | b"XLEN" | b"XPENDING" | b"XRANGE" | b"XREAD" | b"XREVRANGE" | b"ZCARD" | b"ZCOUNT" | b"ZDIFF" | b"ZINTER" | b"ZINTERCARD" | b"ZLEXCOUNT" | b"ZMSCORE" | b"ZRANDMEMBER" | b"ZRANGE" | b"ZRANGEBYLEX" | b"ZRANGEBYSCORE" | b"ZRANK" | b"ZREVRANGE" | b"ZREVRANGEBYLEX" | b"ZREVRANGEBYSCORE" | b"ZREVRANK" | b"ZSCAN" | b"ZSCORE" | b"ZUNION"
+        b"BITCOUNT"
+            | b"BITFIELD_RO"
+            | b"BITPOS"
+            | b"DBSIZE"
+            | b"DUMP"
+            | b"EVALSHA_RO"
+            | b"EVAL_RO"
+            | b"EXISTS"
+            | b"EXPIRETIME"
+            | b"FCALL_RO"
+            | b"GEODIST"
+            | b"GEOHASH"
+            | b"GEOPOS"
+            | b"GEORADIUSBYMEMBER_RO"
+            | b"GEORADIUS_RO"
+            | b"GEOSEARCH"
+            | b"GET"
+            | b"GETBIT"
+            | b"GETRANGE"
+            | b"HEXISTS"
+            | b"HGET"
+            | b"HGETALL"
+            | b"HKEYS"
+            | b"HLEN"
+            | b"HMGET"
+            | b"HRANDFIELD"
+            | b"HSCAN"
+            | b"HSTRLEN"
+            | b"HVALS"
+            | b"KEYS"
+            | b"LCS"
+            | b"LINDEX"
+            | b"LLEN"
+            | b"LOLWUT"
+            | b"LPOS"
+            | b"LRANGE"
+            | b"MEMORY USAGE"
+            | b"MGET"
+            | b"OBJECT ENCODING"
+            | b"OBJECT FREQ"
+            | b"OBJECT IDLETIME"
+            | b"OBJECT REFCOUNT"
+            | b"PEXPIRETIME"
+            | b"PFCOUNT"
+            | b"PTTL"
+            | b"RANDOMKEY"
+            | b"SCAN"
+            | b"SCARD"
+            | b"SDIFF"
+            | b"SINTER"
+            | b"SINTERCARD"
+            | b"SISMEMBER"
+            | b"SMEMBERS"
+            | b"SMISMEMBER"
+            | b"SORT_RO"
+            | b"SRANDMEMBER"
+            | b"SSCAN"
+            | b"STRLEN"
+            | b"SUBSTR"
+            | b"SUNION"
+            | b"TOUCH"
+            | b"TTL"
+            | b"TYPE"
+            | b"XINFO CONSUMERS"
+            | b"XINFO GROUPS"
+            | b"XINFO STREAM"
+            | b"XLEN"
+            | b"XPENDING"
+            | b"XRANGE"
+            | b"XREAD"
+            | b"XREVRANGE"
+            | b"ZCARD"
+            | b"ZCOUNT"
+            | b"ZDIFF"
+            | b"ZINTER"
+            | b"ZINTERCARD"
+            | b"ZLEXCOUNT"
+            | b"ZMSCORE"
+            | b"ZRANDMEMBER"
+            | b"ZRANGE"
+            | b"ZRANGEBYLEX"
+            | b"ZRANGEBYSCORE"
+            | b"ZRANK"
+            | b"ZREVRANGE"
+            | b"ZREVRANGEBYLEX"
+            | b"ZREVRANGEBYSCORE"
+            | b"ZREVRANK"
+            | b"ZSCAN"
+            | b"ZSCORE"
+            | b"ZUNION"
     )
 }
 
@@ -1791,7 +1881,7 @@ implement_commands! {
     ///     STREAMS key_1 key_2 ... key_N
     ///     ID_1 ID_2 ... ID_N
     ///
-    /// XREADGROUP [GROUP group-name consumer-name] [BLOCK <milliseconds>] [COUNT <count>] [NOACK] 
+    /// XREADGROUP [GROUP group-name consumer-name] [BLOCK <milliseconds>] [COUNT <count>] [NOACK]
     ///     STREAMS key_1 key_2 ... key_N
     ///     ID_1 ID_2 ... ID_N
     /// ```
