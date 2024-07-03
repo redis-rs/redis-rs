b954618f4af2730b42366f7caa83ea76d054ba23
barshaul <barshaul@amazon.com>
2024-07-03 17:31:03 +0000

    Call to refresh_connections only if failed_conn isn't empty

--- redis/src/cluster_async/mod.rs
+++ redis/src/cluster_async/mod.rs
@@ -1448,12 +1448,14 @@ where
         }
         drop(read_guard);
 
-        Self::refresh_connections(
-            inner,
-            failed_connections,
-            RefreshConnectionType::OnlyManagementConnection,
-        )
-        .await;
+        if !failed_connections.is_empty() {
+            Self::refresh_connections(
+                inner,
+                failed_connections,
+                RefreshConnectionType::OnlyManagementConnection,
+            )
+            .await;
+        }
 
         false
     }
