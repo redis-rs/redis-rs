c4cf58851ea4e6fa8e8a1ae33f2b823f32767ecc
Jonas Meier <jonas.meier@ergon.ch>
2025-02-07 16:50:12 +0100

    Add test test_sentinel_client_builder

--- redis/tests/support/sentinel.rs
+++ redis/tests/support/sentinel.rs
@@ -59,19 +59,21 @@ impl TestSentinelContext {
 
     pub fn sentinel_node_connection_info(&self) -> SentinelNodeConnectionInfo {
         SentinelNodeConnectionInfo {
-            tls_mode: if let ConnectionAddr::TcpTls { insecure, .. } =
-                self.cluster.servers[0].client_addr()
-            {
-                if *insecure {
-                    Some(TlsMode::Insecure)
-                } else {
-                    Some(TlsMode::Secure)
-                }
-            } else {
-                None
-            },
+            tls_mode: self.tls_mode(),
             redis_connection_info: None,
             certs: None,
+        }
+    }
+
+    pub fn tls_mode(&self) -> Option<TlsMode> {
+        if let ConnectionAddr::TcpTls { insecure, .. } = self.cluster.servers[0].client_addr() {
+            if *insecure {
+                Some(TlsMode::Insecure)
+            } else {
+                Some(TlsMode::Secure)
+            }
+        } else {
+            None
         }
     }
 
