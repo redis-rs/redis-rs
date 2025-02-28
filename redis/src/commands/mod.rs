9e80f8b0b9164d23748ebec44661a21fbce9bbd0
Murray Groves <github@murraygrov.es>
2025-02-28 21:20:01 +0000

    Change `isize` types to `IntegerReplyOrNoOp` where relevant

--- redis/src/commands/mod.rs
+++ redis/src/commands/mod.rs
@@ -253,14 +253,14 @@ implement_commands! {
     }
 
     /// Get the absolute Unix expiration timestamp in seconds.
-    /// Returns -1 if key exists but has no expiration time, -2 if key does not exist.
-    fn expire_time<K: ToRedisArgs>(key: K) -> isize {
+	/// Returns `ExistsButNotRelevant` if key exists but has no expiration time.
+    fn expire_time<K: ToRedisArgs>(key: K) -> IntegerReplyOrNoOp {
         cmd("EXPIRETIME").arg(key)
     }
 
     /// Get the absolute Unix expiration timestamp in milliseconds.
-    /// Returns -1 if key exists but has no expiration time, -2 if key does not exist.
-    fn pexpire_time<K: ToRedisArgs>(key: K) -> isize {
+	/// Returns `ExistsButNotRelevant` if key exists but has no expiration time,.
+    fn pexpire_time<K: ToRedisArgs>(key: K) -> IntegerReplyOrNoOp {
         cmd("PEXPIRETIME").arg(key)
     }
 
@@ -271,13 +271,13 @@ implement_commands! {
     }
 
     /// Get the time to live for a key in seconds.
-    /// Returns -1 if key exists but has no expiration time, -2 if key does not exist.
-    fn ttl<K: ToRedisArgs>(key: K) -> isize {
+	/// Returns `ExistsButNotRelevant` if key exists but has no expiration time.
+    fn ttl<K: ToRedisArgs>(key: K) -> IntegerReplyOrNoOp {
         cmd("TTL").arg(key)
     }
 
     /// Get the time to live for a key in milliseconds.
-    /// Returns -1 if key exists but has no expiration time, -2 if key does not exist.
+	/// Returns `ExistsButNotRelevant` if key exists but has no expiration time.
     fn pttl<K: ToRedisArgs>(key: K) -> isize {
         cmd("PTTL").arg(key)
     }
