32799339292beae231931d640e9daaa299781193
Murray Groves <github@murraygrov.es>
2025-03-07 20:11:37 +0000

    Apply cargo fmt

--- redis/src/lib.rs
+++ redis/src/lib.rs
@@ -610,7 +610,8 @@ pub use crate::types::{
 #[cfg(feature = "aio")]
 #[cfg_attr(docsrs, doc(cfg(feature = "aio")))]
 pub use crate::{
-    cmd::AsyncIter, commands::AsyncCommands, commands::AsyncTypedCommands, parser::parse_redis_value_async, types::RedisFuture,
+    cmd::AsyncIter, commands::AsyncCommands, commands::AsyncTypedCommands,
+    parser::parse_redis_value_async, types::RedisFuture,
 };
 
 mod macros;
