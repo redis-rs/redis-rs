42cd67c21c25d4e72d9a9dc412ed94c0a7b6fd26
Shachar Langbeheim <nihohit@gmail.com>
2025-03-25 23:39:37 +0200

    re-export socket2. (#1573)
    https://github.com/redis-rs/redis-rs/issues/1571

--- redis/src/io/tcp.rs
+++ redis/src/io/tcp.rs
@@ -3,6 +3,8 @@ use std::{io, net::TcpStream};
 #[cfg(any(target_os = "android", target_os = "fuchsia", target_os = "linux"))]
 use std::time::Duration;
 
+pub use socket2;
+
 /// Settings for a TCP stream.
 #[derive(Clone, Debug)]
 pub struct TcpSettings {
