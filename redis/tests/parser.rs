ae382103491a9db3e419474a2ec4ecb7b50493a6
Shachar Langbeheim <nihohit@gmail.com>
2025-04-15 20:17:36 +0300

    Update redis/tests/parser.rs
    Co-authored-by: Altan Ozlu <5479094+altanozlu@users.noreply.github.com>

--- redis/tests/parser.rs
+++ redis/tests/parser.rs
@@ -188,7 +188,7 @@ quickcheck! {
         let mut partial_reader = PartialAsyncRead { inner: &mut reader, ops: Box::new(seq.into_iter()) };
         let mut decoder = combine::stream::Decoder::new();
 
-        let result =  current_thread_runtime().block_on(redis::parse_redis_value_async(&mut decoder, &mut partial_reader));
+        let result = current_thread_runtime().block_on(redis::parse_redis_value_async(&mut decoder, &mut partial_reader));
         assert!(result.as_ref().is_ok(), "{}", result.unwrap_err());
         assert_eq!(
             result.unwrap(),
