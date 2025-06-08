58e9942fcc8bd816cc63e46ad51df455e5e543ff
Shachar Langbeheim <nihohit@gmail.com>
2025-06-08 19:41:59 +0300

    Remove unsafe iterators. (#1660)
    * Prepare 1.0.x branch for usage.
    
    * t
    
    * Remove unsafe iterators.

--- redis/tests/test_basic.rs
+++ redis/tests/test_basic.rs
@@ -1187,7 +1187,6 @@ mod basic {
             .iter(&mut con)
             .unwrap();
 
-        #[cfg(feature = "safe_iterators")]
         let iter = iter.map(std::result::Result::unwrap);
 
         for x in iter {
@@ -1198,7 +1197,6 @@ mod basic {
         assert_eq!(unseen.len(), 0);
     }
 
-    #[cfg(feature = "safe_iterators")]
     #[test]
     fn test_checked_scanning_error() {
         const KEY_COUNT: u32 = 1000;
@@ -1270,7 +1268,6 @@ mod basic {
             .hscan_match::<&str, &str, (String, usize)>("foo", "key_0_*")
             .unwrap();
 
-        #[cfg(feature = "safe_iterators")]
         let iter = iter.map(std::result::Result::unwrap);
 
         for (_field, value) in iter {
@@ -2096,7 +2093,6 @@ mod basic {
         let iter: redis::Iter<'_, (String, isize)> = con.hscan("my_hash").unwrap();
         let mut found = HashSet::new();
 
-        #[cfg(feature = "safe_iterators")]
         let iter = iter.map(std::result::Result::unwrap);
 
         for item in iter {
@@ -2179,7 +2175,6 @@ mod basic {
 
         let mut counter = 0;
         for kv in iter {
-            #[cfg(feature = "safe_iterators")]
             let kv = kv.unwrap();
 
             let (key, num) = kv;
