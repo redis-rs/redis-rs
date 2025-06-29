e1533297c5206e38ffab66c00ae25ebbba4105cd
Shachar Langbeheim <nihohit@gmail.com>
2025-06-29 20:39:35 +0300

    Fill out  ServerError.

--- redis/src/errors/server_error.rs
+++ redis/src/errors/server_error.rs
@@ -1,17 +1,32 @@
-#[derive(PartialEq, Debug, Clone, Copy)]
+use std::fmt;
+
+#[derive(PartialEq, Debug, Clone, Copy, Eq)]
 pub enum ServerErrorKind {
+    /// The server generated an invalid response.
     ResponseError,
+    /// A script execution was aborted.
     ExecAbortError,
+    /// The server cannot response because it's loading a dump.
     BusyLoadingError,
+    /// A script that was requested does not actually exist.
     NoScriptError,
+    /// Raised if a key moved to a different node.
     Moved,
+    /// Raised if a key moved to a different node but we need to ask.
     Ask,
+    /// Raised if a request needs to be retried.
     TryAgain,
+    /// Raised if a redis cluster is down.
     ClusterDown,
+    /// A request spans multiple slots
     CrossSlot,
+    /// A cluster master is unavailable.
     MasterDown,
+    /// Attempt to write to a read-only server
     ReadOnly,
+    /// Attempted to kill a script/function while they werent' executing
     NotBusy,
+    /// Attempted to unsubscribe on a connection that is not in subscribed mode.
     NoSub,
 }
 
@@ -60,6 +75,29 @@ impl ServerError {
         match self {
             ServerError::ExtensionError { detail, .. } => detail.as_ref().map(|str| str.as_str()),
             ServerError::KnownError { detail, .. } => detail.as_ref().map(|str| str.as_str()),
+        }
+    }
+}
+
+impl fmt::Display for ServerError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match &self {
+            ServerError::ExtensionError { code, detail } => {
+                fmt::Debug::fmt(&code, f)?;
+                if let Some(detail) = detail {
+                    f.write_str(": ")?;
+                    detail.fmt(f)?;
+                }
+                Ok(())
+            }
+            ServerError::KnownError { kind, detail } => {
+                fmt::Debug::fmt(&kind, f)?;
+                if let Some(detail) = detail {
+                    f.write_str(": ")?;
+                    detail.fmt(f)?;
+                }
+                Ok(())
+            }
         }
     }
 }
