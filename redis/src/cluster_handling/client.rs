2d0d8f894b71165ca8284f8324b613343c54a2ef
Markus Westerlind <markus.westerlind@imperva.com>
2025-11-17 15:14:03 +0100

    doc: Correct get_pending_async_connect_with_config doc comment

--- redis/src/cluster_handling/client.rs
+++ redis/src/cluster_handling/client.rs
@@ -572,12 +572,8 @@ impl ClusterClient {
     }
 
     /// Creates new connections to Redis Cluster nodes with a custom config and returns a
-    /// [`cluster_async::ClusterConnection`]. The connections to the cluster are done in the
-    /// background.
-    ///
-    /// # Errors
-    ///
-    /// An error is returned if there is a failure while creating connections or slots.
+    /// [`cluster_async::ClusterConnection`]. The connections to the cluster nodes are done in the
+    /// background so the caller won't know if the cluster is available until the first command is sent.
     #[cfg(feature = "cluster-async")]
     pub fn get_pending_async_connection_with_config(
         &self,
