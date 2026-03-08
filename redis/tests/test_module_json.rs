fcf0249509d9ce8bb5dcd6471e20e05f1023b2e2
Christian Aistleitner <christian@quelltextlich.at>
2026-03-08 10:38:13 +0100

    ci: Make RESP3 flag effective for JSON module tests
    While the `Makefile` set `RESP3=true`, this setting never got picked up,
    as the correct way to flag RESP3 use is `PROTOCOL=RESP3`. We switch to
    that.
    
    Additionally, the `REDIS_VERSION` environment variable gets dropped, as
    the version check was off (it only detected version 7, not 8, 9 as RESP3
    users) and tests hence failed. We switch to the canonical `Version`,
    which allows proper version comparisons and make RESP3 tests work for
    Redis >=8.

--- redis/tests/test_module_json.rs
+++ redis/tests/test_module_json.rs
@@ -352,8 +352,7 @@ fn test_module_json_num_incr_by() {
 
     assert_eq!(set_initial, Ok(true));
 
-    let redis_ver = std::env::var("REDIS_VERSION").unwrap_or_default();
-    if ctx.protocol.supports_resp3() && redis_ver.starts_with("7.") {
+    if ctx.protocol.supports_resp3() && ctx.get_version() >= REDIS_VERSION_CE_7_0 {
         // cannot increment a string
         let json_numincrby_a: RedisResult<Vec<Value>> = con.json_num_incr_by(TEST_KEY, "$.a", 2);
         assert_eq!(json_numincrby_a, Ok(vec![Nil]));
@@ -505,8 +504,7 @@ fn test_module_json_type() {
     let json_type_b: RedisResult<Value> = con.json_type(TEST_KEY, "$..a");
     let json_type_c: RedisResult<Value> = con.json_type(TEST_KEY, "$..dummy");
 
-    let redis_ver = std::env::var("REDIS_VERSION").unwrap_or_default();
-    if ctx.protocol.supports_resp3() && redis_ver.starts_with("7.") {
+    if ctx.protocol.supports_resp3() && ctx.get_version() >= REDIS_VERSION_CE_7_0 {
         // In RESP3 current RedisJSON always gives response in an array.
         assert_eq!(
             json_type_a,
