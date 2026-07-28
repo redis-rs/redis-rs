d1140b18a88c3231f67f5654f760cf8031af1612
StefanPalashev <stefan.palashev@redis.com>
2026-07-28 17:41:47 +0300

    Address pull request comments

--- redis-test/src/server.rs
+++ redis-test/src/server.rs
@@ -218,11 +218,11 @@ impl RedisServer {
                     redis_cmd.arg("--loadmodule").arg(path);
                 }
                 Module::Search => {
-                    redis_cmd
-                        .arg("--loadmodule")
-                        .arg(env::var("REDISRS_REDIS_SEARCH_PATH").expect(
+                    let path = env::var("REDISRS_REDIS_SEARCH_PATH").expect(
                         "Unable to find path to RediSearch at REDISRS_REDIS_SEARCH_PATH, is it set?",
-                    ));
+                    );
+
+                    redis_cmd.arg("--loadmodule").arg(path);
                 }
             };
         }
