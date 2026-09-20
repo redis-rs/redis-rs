faecfb73c891ee9ba5173c10b9c4078eb19f8c57
Shachar Langbeheim <nihohit@gmail.com>
2026-09-20 15:19:33 +0300

    test-macros: Add sentinel matrix test macros
    Add the sentinel_test and async_sentinel_test attributes, generating the full
    protocol x server-type x runtime matrix for sentinel tests.

--- test-macros/src/lib.rs
+++ test-macros/src/lib.rs
@@ -1,6 +1,7 @@
 use proc_macro::TokenStream;
 
 mod cluster;
+mod sentinel;
 mod single;
 mod test_env;
 mod utils;
@@ -28,4 +29,14 @@ pub fn cluster_test(attr: TokenStream, input: TokenStream) -> TokenStream {
 #[proc_macro_attribute]
 pub fn async_cluster_test(attr: TokenStream, input: TokenStream) -> TokenStream {
     cluster::expand_async_cluster_test(attr.into(), input.into()).into()
+}
+
+#[proc_macro_attribute]
+pub fn sentinel_test(attr: TokenStream, input: TokenStream) -> TokenStream {
+    sentinel::expand_sentinel_test(attr.into(), input.into()).into()
+}
+
+#[proc_macro_attribute]
+pub fn async_sentinel_test(attr: TokenStream, input: TokenStream) -> TokenStream {
+    sentinel::expand_async_sentinel_test(attr.into(), input.into()).into()
 }
