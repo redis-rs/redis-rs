094830125120c849f07a3b8b5ab479839de7839c
Shachar Langbeheim <nihohit@gmail.com>
2026-09-20 15:19:22 +0300

    test-macros: Add single-server matrix test macros and codegen infrastructure
    Replace the env-var-driven runtime selection in test_macros with macros that
    generate the full protocol x server-type test matrix. Add the shared codegen
    infrastructure (test_env, utils) and the single-server family:
    single_server_test, async_single_server_test and async_test.
    
    Switch from syn 3 to syn 2 and add rstest as dev-dependency to test the
    generated code, updating Cargo.lock accordingly. Wire the crate's unit tests
    into the Makefile test target.

--- test-macros/src/lib.rs
+++ test-macros/src/lib.rs
@@ -1,102 +1,20 @@
 use proc_macro::TokenStream;
-use quote::quote;
+
+mod single;
+mod test_env;
+mod utils;
 
 #[proc_macro_attribute]
-pub fn async_test(_: TokenStream, input: TokenStream) -> TokenStream {
-    let mut item = syn::parse_macro_input!(input as syn::ItemFn);
+pub fn single_server_test(attr: TokenStream, input: TokenStream) -> TokenStream {
+    single::expand_single_server_test(attr.into(), input.into()).into()
+}
 
-    let test_function_name = item.sig.ident.clone();
+#[proc_macro_attribute]
+pub fn async_single_server_test(attr: TokenStream, input: TokenStream) -> TokenStream {
+    single::expand_async_single_server_test(attr.into(), input.into()).into()
+}
 
-    item.sig.ident = syn::Ident::new(
-        &format!("{test_function_name}_internal"),
-        test_function_name.span(),
-    );
-    let function_name = item.sig.ident.clone();
-
-    let final_code = if item.sig.inputs.len() == 1 {
-        let Some(syn::FnArg::Typed(pat_type)) = item.sig.inputs.last() else {
-            return syn::Error::new_spanned(&item.sig.inputs, "Expected typed argument")
-                .to_compile_error()
-                .into();
-        };
-        // Verify the argument is a boolean
-        let is_bool = matches!(&*pat_type.ty, syn::Type::Path(type_path)
-                if type_path.path.is_ident("bool"));
-        let is_connection = matches!(&*pat_type.ty, syn::Type::ImplTrait(impl_trait)
-        if impl_trait.bounds.iter().any(|bound| {
-            matches!(bound, syn::TypeParamBound::Trait(trait_bound)
-                if trait_bound.path.segments.last().is_some_and(|seg| seg.ident == "ConnectionLike" || seg.ident == "AsyncTypedCommands" || seg.ident == "AsyncCommands"))
-        }));
-
-        if is_connection {
-            quote! {
-                mod #test_function_name {
-                    use super::*;
-                    #item
-
-                    #[rstest::rstest]
-                    #[cfg_attr(feature = "tokio-comp", case::tokio(support::RuntimeType::Tokio))]
-                    #[cfg_attr(feature = "smol-comp", case::smol(support::RuntimeType::Smol))]
-                    fn multiplexed_connection (#[case]runtime: support::RuntimeType) {
-                        let ctx = TestContext::default();
-                        support::block_on_all(async move {
-                            let conn = ctx.async_connection().await.unwrap();
-                            #function_name (conn).await
-                        }, runtime);
-                    }
-
-                    #[rstest::rstest]
-                    #[cfg_attr(feature = "tokio-comp", case::tokio(support::RuntimeType::Tokio))]
-                    #[cfg_attr(feature = "smol-comp", case::smol(support::RuntimeType::Smol))]
-                    #[cfg(feature = "connection-manager")]
-                    fn connection_manager (#[case]runtime: support::RuntimeType) {
-                        let ctx = TestContext::default();
-                        support::block_on_all(async move {
-                            let conn = ctx.client.get_connection_manager().await.unwrap();
-                            #function_name (conn).await
-                        }, runtime);
-                    }
-
-                }
-            }
-        } else if is_bool {
-            quote! {
-                #item
-
-                #[rstest::rstest]
-                #[cfg_attr(feature = "tokio-comp", case::tokio(RuntimeType::Tokio))]
-                #[cfg_attr(feature = "smol-comp", case::smol(RuntimeType::Smol))]
-                fn #test_function_name (#[case]runtime: RuntimeType, #[values(true, false)] arg: bool) {
-                    block_on_all(#function_name (arg), runtime);
-                }
-            }
-        } else {
-            return syn::Error::new_spanned(
-                &pat_type.ty,
-                format!("Unsupported argument type. arg pattern is: {pat_type:?}"),
-            )
-            .to_compile_error()
-            .into();
-        }
-    } else if item.sig.inputs.is_empty() {
-        quote! {
-            #item
-
-            #[rstest::rstest]
-            #[cfg_attr(feature = "tokio-comp", case::tokio(RuntimeType::Tokio))]
-            #[cfg_attr(feature = "smol-comp", case::smol(RuntimeType::Smol))]
-            fn #test_function_name (#[case]runtime: RuntimeType) {
-                block_on_all(#function_name (), runtime);
-            }
-        }
-    } else {
-        return syn::Error::new_spanned(
-            &item.sig.inputs,
-            "Unsupported number of arguments in function",
-        )
-        .to_compile_error()
-        .into();
-    };
-
-    final_code.into()
+#[proc_macro_attribute]
+pub fn async_test(attr: TokenStream, input: TokenStream) -> TokenStream {
+    single::expand_async_single_server_test(attr.into(), input.into()).into()
 }
