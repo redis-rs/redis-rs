a4665d6e4da6779123f1a8a0575187dc66f2b782
Shachar Langbeheim <nihohit@gmail.com>
2026-09-20 15:19:28 +0300

    test-macros: Add cluster matrix test macros
    Add the cluster_test and async_cluster_test attributes, generating the full
    protocol x server-type x runtime matrix for cluster tests, with optional
    config, supported_versions and database_id attribute arguments.

--- test-macros/src/lib.rs
+++ test-macros/src/lib.rs
@@ -1,5 +1,6 @@
 use proc_macro::TokenStream;
 
+mod cluster;
 mod single;
 mod test_env;
 mod utils;
@@ -17,4 +18,14 @@ pub fn async_single_server_test(attr: TokenStream, input: TokenStream) -> TokenS
 #[proc_macro_attribute]
 pub fn async_test(attr: TokenStream, input: TokenStream) -> TokenStream {
     single::expand_async_single_server_test(attr.into(), input.into()).into()
+}
+
+#[proc_macro_attribute]
+pub fn cluster_test(attr: TokenStream, input: TokenStream) -> TokenStream {
+    cluster::expand_cluster_test(attr.into(), input.into()).into()
+}
+
+#[proc_macro_attribute]
+pub fn async_cluster_test(attr: TokenStream, input: TokenStream) -> TokenStream {
+    cluster::expand_async_cluster_test(attr.into(), input.into()).into()
 }
